36ecb8b588610ea819c02a636abe57e08cc2a827
Eddie Park <eddie.park@id.me>
2015-11-03 18:40:35 -0500

    Add first responder partner scenario to feature
    Updated the scenario to include first responders
    since previous feature didn't have it included
    which I assume is because it wasn't available at
    the time. Also refactored the item to cart into
    a background

--- features/partner_tests/fanatics.feature
+++ features/partner_tests/fanatics.feature
@@ -1,8 +1,15 @@
 @fanatics @partners
 Feature: Test the ID.me Fanatics Partner Integration
 
-  @troop_id @windows @sauce
-  Scenario: Test Fanatics Shop Partner Integration
+  Background:
     * I add an item to the cart ("FAN")
+
+  @troop_id @windows @sauce
+  Scenario: Test Fanatics Shop Military Partner Integration
     * I apply the Troop ID discount ("FAN")
-    * I verify the discount has been applied ("FAN") 
+    * I verify the Troop ID discount has been applied ("FAN")
+
+  @first_responder
+  Scenario: Test Fanatics Shop First Responder Partner Integration
+    * I apply the First Responder discount ("FAN")
+    * I verify the First Responder discount has been applied ("FAN")
--- features/step_definitions/fanatics_steps.rb
+++ features/step_definitions/fanatics_steps.rb
@@ -1,12 +1,20 @@
-Given(/^I add an item to the cart \("([^"]*)"\)$/) do |arg1|
-  pending
-  #visit "http://www.fanatics.com"
-end
 
-Given(/^I apply the Troop ID discount \("([^"]*)"\)$/) do |arg1|
+Given(/^I add an item to the cart \("([^"]*)"\)$/) do |brand|
   pending # Write code here that turns the phrase above into concrete actions
 end
 
-Given(/^I verify the discount has been applied \("([^"]*)"\)$/) do |arg1|
+Given(/^I apply the Troop ID discount \("([^"]*)"\)$/) do |brand|
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Given(/^I verify the Troop ID discount has been applied \("([^"]*)"\)$/) do |brand|
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Given(/^I apply the First Responder discount \("([^"]*)"\)$/) do |brand|
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Given(/^I verify the First Responder discount has been applied \("([^"]*)"\)$/) do |brand|
   pending # Write code here that turns the phrase above into concrete actions
 end
