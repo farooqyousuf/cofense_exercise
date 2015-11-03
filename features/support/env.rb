87edb67b2cdf514f4d5c552592bdef21eac72cca
Farooq <farooqyousuf1@gmail.com>
2015-11-03 16:01:29 -0500

    edited env file and added staging yml file

--- features/support/env.rb
+++ features/support/env.rb
@@ -1,40 +1,10 @@
-# `killall chromedriver`
-#
-# `adb kill-server`
-# `adb devices`
-
 require_relative "dependencies"
-require 'fileutils'
-require 'selenium-webdriver'
-
-server_url = "http://localhost:4723/wd/hub"
-
-capabilities =
-{
-  'platformName' => 'Android',
-  'deviceName' => 'Android Emulator',
-  'platformVersion' => '5.1',
-  'browserName' => 'Chrome'
-}
-
-# caps = Selenium::WebDriver::Remote::Capabilities.android
-# caps['appiumVersion'] = '1.4.13'
-# caps['deviceName'] = 'Android Emulator'
-# caps['deviceOrientation'] = 'portrait'
-# caps['browserName'] = 'Browser'
-# caps['platformVersion'] = '5.1'
-# caps['platformName'] = 'Android
 
 FigNewton.load("staging.yml")
 
-driver = Selenium::WebDriver.for(:remote, :desired_capabilities => capabilities, :url => server_url)
-
-browser = Watir::Browser.new driver
-
-# browser.driver.manage.window.maximize
+browser = Watir::Browser.new :firefox
+browser.driver.manage.window.maximize
 browser.driver.manage.timeouts.implicit_wait = 30
-
-puts "New Android Watir Browser Object Instantiated"
 
 Before do
   @browser = browser
@@ -47,8 +17,7 @@ end
 
 puts "=" * 25
 puts "Environment: Staging"
-puts "Platform: iOS"
-puts "Browser: Safari"
+puts "Browser: Firefox"
 puts "=" * 25
 
 World(PageObject::PageFactory)
