fbc63b8e614a0db4fce93a05c2d5f76c270edcda
Eddie Park <eddie.park@id.me>
2015-11-04 14:16:42 -0500

    Delete old step method helper code

--- features/support/env.rb
+++ features/support/env.rb
@@ -13,7 +13,6 @@ require 'pry'
 #require_relative 'test_helper'
 require 'capybara/cucumber'
 require_relative 'test_helper'
-require_relative 'step_method_helpers'
 # require 'allure-cucumber'
 
 # include AllureCucumber::DSL
