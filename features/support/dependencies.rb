d8b6615986133c904af389e96be1e4949ec87747
Tom Robison <tom@id.me>
2015-11-04 18:16:12 -0500

    Remove currently extra environment configs; consolidate outputs

--- features/support/dependencies.rb
+++ features/support/dependencies.rb
@@ -8,19 +8,5 @@ require "allure-cucumber"
 require "data_magic"
 require "fig_newton"
 require "rspec/expectations"
-require 'fig_newton'
-require 'pry'
-require 'capybara/cucumber'
+require "capybara/cucumber"
 require_relative 'test_helper'
-# require 'allure-cucumber'
-
-# DATA  ||= DataMagic.load("data.yml")
-# KBA   ||= YAML.load_file("config/data/kba.yml")
-#
-# include AllureCucumber::DSL
-#
-# AllureCucumber.configure do |c|
-#   c.output_dir = "output/allure"
-# end
-#
-# PageObject.javascript_framework = :jquery
