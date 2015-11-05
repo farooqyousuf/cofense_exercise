4fff8af9b264fc0c05bdbfd64e79c375bb11d4ff
Eddie Park <eddie.park@id.me>
2015-11-05 11:41:13 -0500

    Add passing code for seaworld step definitions
    All current tests pass with addition to content
    expectation to verify that is specific with TroopID
    "Military Ticket Offers"

--- features/step_definitions/seaworld_steps.rb
+++ features/step_definitions/seaworld_steps.rb
@@ -1,11 +1,18 @@
 Given(/^I visit the waves of honor seaworld site$/) do
-  pending # Write code here that turns the phrase above into concrete actions
+  visit "http://seaworldparks.com/seaworld-orlando/waves-of-honor/active/"
 end
 
 Given(/^I log in with my ID\.me military account$/) do
-  pending # Write code here that turns the phrase above into concrete actions
+  idme_window = window_opened_by do
+    find(".IDmeLoginButton").click
+  end
+
+  within_window idme_window do
+   sign_in_with_idme
+  end
 end
 
 Given(/^I verify access to discounted tickets$/) do
-  pending # Write code here that turns the phrase above into concrete actions
+  expect(page).to have_content("Military Ticket Offers")
+  expect(page).to have_content("Thank you for your service. Please see all offers available for you below.")
 end
