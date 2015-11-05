7c3c42f443fc18373b57be91df88882231a8366a
Eddie Park <eddie.park@id.me>
2015-11-05 11:30:03 -0500

    Add seaworld features and outline step defs

---
+++ features/step_definitions/seaworld_steps.rb
@@ -0,0 +1,11 @@
+Given(/^I visit the waves of honor seaworld site$/) do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Given(/^I log in with my ID\.me military account$/) do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Given(/^I verify access to discounted tickets$/) do
+  pending # Write code here that turns the phrase above into concrete actions
+end
