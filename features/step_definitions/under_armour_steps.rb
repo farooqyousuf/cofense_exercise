78b6ac87f5c3843cc0ea5ca1e1c60559afa5ebc8
Eddie Park <eddie.park@id.me>
2015-11-06 10:51:22 -0500

    CORE-3537: Add items to the cart

--- features/step_definitions/under_armour_steps.rb
+++ features/step_definitions/under_armour_steps.rb
@@ -1,11 +1,18 @@
 Given(/^UA \- I add an item to the cart$/) do
-  pending # Write code here that turns the phrase above into concrete actions
+  visit "http://www.underarmour.com/en-us/technology/armour/armourvent/mens?iid=hero"
+  find(".close").click
+  first(".product-img-link").click
+  first(".size-chip").click
+  find(".addtocart-btn").click
+  find(".checkout-btn").click
+  first(".checkout-btn").click
 end
 
-Given(/^UA \- I apply the "([^"]*)" discount$/) do |arg1|
-  pending # Write code here that turns the phrase above into concrete actions
+Given(/^UA \- I apply the "([^"]*)" discount$/) do |type|
+ pending
+
 end
 
-Given(/^UA \- I verify the "([^"]*)" discount has been applied$/) do |arg1|
-  pending # Write code here that turns the phrase above into concrete actions
+Given(/^UA \- I verify the "([^"]*)" discount has been applied$/) do |type|
+ pending
 end
