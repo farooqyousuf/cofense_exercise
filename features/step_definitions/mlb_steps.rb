57d452188a163e4d5260b9e1c1254a8e768a529d
Eddie Park <eddie.park@id.me>
2015-11-06 12:12:49 -0500

    CORE-3538:Add feature stories & step outlines

---
+++ features/step_definitions/mlb_steps.rb
@@ -0,0 +1,11 @@
+Given(/^MLB \- I add an item to the cart$/) do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Given(/^MLB \- I apply the Troop ID discount$/) do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Given(/^MLB \- I verify the Troop ID discount has been applied$/) do
+  pending # Write code here that turns the phrase above into concrete actions
+end
