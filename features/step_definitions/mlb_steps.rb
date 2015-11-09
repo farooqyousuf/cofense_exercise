0170bc940a6161ee2db9cd78c70299a585408410
Eddie Park <eddie.park@id.me>
2015-11-09 10:57:03 -0500

    Clean up syntax for add item to cart step def

--- features/step_definitions/mlb_steps.rb
+++ features/step_definitions/mlb_steps.rb
@@ -1,7 +1,7 @@
 Given(/^MLB \- I add an item to the cart$/) do
   visit "http://mlb.mlb.com/mlb/subscriptions/index.jsp"
-  click_link("Get MLB.TV")
-  click_on("Buy Now!")
+  click_link "Get MLB.TV"
+  click_on "Buy Now!"
 end
 
 Given(/^MLB \- I apply the Troop ID discount$/) do
