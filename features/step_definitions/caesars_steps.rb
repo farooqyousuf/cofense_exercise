8cf6297a45be5576adb8a18f9ef501747f21ec09
Eddie Park <eddie.park@id.me>
2015-11-09 17:25:54 -0500

    CORE-3540: Add step to Sign-in and Verify TroopID

--- features/step_definitions/caesars_steps.rb
+++ features/step_definitions/caesars_steps.rb
@@ -4,15 +4,17 @@ Given(/^CS \- I add an item to the cart$/) do
 end
 
 Given(/^CS \- I apply the Troop ID discount$/) do
-  idme_window = window_opened_by do
+  @idme_window = window_opened_by do
     first(".idme-btn-container a").native.send_key:return
   end
 
-  within_window idme_window do
+  within_window @idme_window do
     sign_in_with_idme
   end
 end
 
 Given(/^CS \- I verify the discount has been applied$/) do
-  pending # Write code here that turns the phrase above into concrete actions
+  within_window @idme_window do
+    expect(page.has_xpath?("//img[@alt='id.me applied']")).to be true
+  end
 end
