c2a066d9d129c1d3fc75679a3a20d1ad1d426256
Eddie Park <eddie.park@id.me>
2015-11-09 12:36:20 -0500

    CORE-3539: Create step def file and add step def outlineS

---
+++ features/step_definitions/jockey_steps.rb
@@ -0,0 +1,11 @@
+Given(/^JO \- I add an item to the cart$/) do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Given(/^JO \- I apply the Troop ID discount$/) do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Given(/^JO \- I verify the Troop ID discount has been applied$/) do
+  pending # Write code here that turns the phrase above into concrete actions
+end
