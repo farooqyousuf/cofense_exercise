cbf83ca00662b11ca3c332dc1b9d6995da0a6f0b
Farooq <farooqyousuf1@gmail.com>
2015-11-10 14:25:10 -0500

    minor edit trying to get screenshots working for allure reports on jenkins - 3

--- features/support/dependencies.rb
+++ features/support/dependencies.rb
@@ -5,15 +5,9 @@ require "rspec"
 require "cucumber"
 require "cucumber_statistics/autoload"
 require "allure-cucumber"
+require "allure-cucumber/dsl"
 require "data_magic"
 require "fig_newton"
 require "rspec/expectations"
 require "capybara/cucumber"
 require_relative 'test_helper'
-
-include Capybara::DSL
-include AllureCucumber::DSL
-
-AllureCucumber.configure do |c|
-   c.output_dir = "output/allure"
-end
