d6c694fa60ab130e1d8559e67011686bf36cfa43
Eddie Park <eddie.park@id.me>
2015-11-11 17:00:23 -0500

    CORE-3610: Remove lint from Rubocop

--- features/step_definitions/fansedge_steps.rb
+++ features/step_definitions/fansedge_steps.rb
@@ -2,7 +2,7 @@ Given(/^FE \- I add an item to the cart$/) do
   visit "http://www.fansedge.com/New_England_Patriots_Merchandise/Nike_Tom_Brady_New_England_Patriots_Navy_Blue_Silver_Game_Jersey"
   click_link("Choose Size S")
   find("#addToCart").click
-  first(:link,"Checkout").click
+  first(:link, "Checkout").click
 end
 
 Given(/^FE \- I apply the Troop ID discount$/) do
@@ -12,7 +12,6 @@ Given(/^FE \- I apply the Troop ID discount$/) do
   within_window new_window do
     sign_in_with_idme
   end
-
 end
 
 Given(/^FE \- I verify the Troop ID discount has been applied$/) do
