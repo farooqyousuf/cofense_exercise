8c775c05315eab3abd42624d496ea565f9daaa34
Eddie Park <eddie.park@id.me>
2015-11-11 16:07:08 -0500

    CORE-3610: Create fansEdge step def file

---
+++ features/step_definitions/fansedge_steps.rb
@@ -0,0 +1,11 @@
+Given(/^FE \- I add an item to the cart$/) do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Given(/^FE \- I apply the Troop ID discount$/) do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Given(/^FE \- I verify the Troop ID discount has been applied$/) do
+  pending # Write code here that turns the phrase above into concrete actions
+end
