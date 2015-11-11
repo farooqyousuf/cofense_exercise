787f38ed513d67ed560e75f0fbf2592a8c9c15ea
Farooq <farooqyousuf1@gmail.com>
2015-11-10 23:30:59 -0500

    CORE-3605: Allure reports are working with Capybara tests on Jenkins, screenshots still not working

--- features/support/dependencies.rb
+++ features/support/dependencies.rb
@@ -9,10 +9,10 @@ require "data_magic"
 require "fig_newton"
 require "rspec/expectations"
 require "capybara/cucumber"
-require_relative 'test_helper'
+require_relative "test_helper"
 
 include AllureCucumber::DSL
 
 AllureCucumber.configure do |c|
-   c.output_dir = "output/allure"
+  c.output_dir = "output/allure"
 end
