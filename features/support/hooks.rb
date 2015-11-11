503a3ec5ad9f2d9b1b929c8a3df95278805ec1b1
Farooq <farooqyousuf1@gmail.com>
2015-11-10 23:22:33 -0500

    CORE-3605: Allure reports for Jenkins

--- features/support/hooks.rb
+++ features/support/hooks.rb
@@ -1,18 +1,18 @@
 # NOTE: order matters here, hooks defined last will be ran first
 
 #Actions performed before each scenario
-After do |scenario|
-  if scenario.failed?
+# After do |scenario|
+#   if scenario.failed?
 
-    file = "FAILED_#{scenario.name.gsub(" ","_").gsub(/[^0-9A-Za-z_]/, "")}.png"
+#     file = "FAILED_#{scenario.name.gsub(" ","_").gsub(/[^0-9A-Za-z_]/, "")}.png"
 
-    # create directory for images
-    Dir.mkdir("./output") unless Dir.exists?("./output")
+#     # create directory for images
+#     Dir.mkdir("./output") unless Dir.exists?("./output")
 
-    # save the file locally
-    page.save_screenshot("./output/#{file}")
+#     # save the file locally
+#     page.save_screenshot("./output/#{file}")
 
-    # This should attach screenshots to the Allure reports, but it's not working currently...
-    attach_file(file, File.open("./output/#{file}"))
-  end
-end
+#     # This should attach screenshots to the Allure reports, but it's not working currently...
+#     attach_file(file, File.open("./output/#{file}"))
+#   end
+# end
