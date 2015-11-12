e66c0f09b40284c91e3f3fafb3d0137354c03d51
Eddie Park <eddie.park@id.me>
2015-11-12 10:57:33 -0500

    CORE-3610: Update fansedge url to production.yml file

--- features/step_definitions/fansedge_steps.rb
+++ features/step_definitions/fansedge_steps.rb
@@ -1,5 +1,5 @@
 Given(/^FE \- I add an item to the cart$/) do
-  visit "http://www.fansedge.com/New_England_Patriots_Merchandise/Nike_Tom_Brady_New_England_Patriots_Navy_Blue_Silver_Game_Jersey"
+  visit FigNewton.partners.fansedge
   click_link("Choose Size S")
   find("#addToCart").click
   first(:link, "Checkout").click
