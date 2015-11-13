8d9a795fd1190e49845dd8a80e223bc032bcd5ab
Eddie Park <eddie.park@id.me>
2015-11-13 14:29:44 -0500

    CORE-3624: Add nba step def outline and file

---
+++ features/step_definitions/partner_steps/nba_steps.rb
@@ -0,0 +1,12 @@
+
+Given(/^NBA \- I add an item to the cart$/) do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Given(/^NBA \- I apply the Troop ID discount$/) do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Given(/^NBA \- I verify the Troop ID discount has been applied$/) do
+  pending # Write code here that turns the phrase above into concrete actions
+end
