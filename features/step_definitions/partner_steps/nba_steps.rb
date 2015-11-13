43c560f02e99f7ed5a8557fac9a2efa62eb0cc42
Eddie Park <eddie.park@id.me>
2015-11-13 14:40:32 -0500

    CORE-3624: Add NBA shop item to cart step

--- features/step_definitions/partner_steps/nba_steps.rb
+++ features/step_definitions/partner_steps/nba_steps.rb
@@ -1,6 +1,11 @@
 
 Given(/^NBA \- I add an item to the cart$/) do
-  pending # Write code here that turns the phrase above into concrete actions
+  visit "http://store.nba.com/Boston_Celtics_Mens/Mens_Boston_Celtics_Larry_Bird_adidas_Kelly_Green_Hardwood_Classics_Swingman_Jersey"
+  if page.has_link?("lightboxSaleCloseLink")
+    fanatics_lightbox("NBA")
+  end
+  click_link "Choose Size S"
+  find("#addToCart").click
 end
 
 Given(/^NBA \- I apply the Troop ID discount$/) do
