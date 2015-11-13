cbc976cc4c59684cace6cfd5ae50338376333c3a
Eddie Park <eddie.park@id.me>
2015-11-13 14:18:40 -0500

    Add NHL apply Troop ID discount and Verify steps

--- features/step_definitions/partner_steps/nhl_steps.rb
+++ features/step_definitions/partner_steps/nhl_steps.rb
@@ -1,13 +1,18 @@
 Given(/^NHL \- I add a item to the cart$/) do
-  visit 'http://shop.nhl.com/Boston_Bruins_Jerseys/Mens_Boston_Bruins_Reebok_Black_2016_Winter_Classic_Premier_Jersey'
+  visit "http://shop.nhl.com/Boston_Bruins_Jerseys/Mens_Boston_Bruins_Reebok_Black_2016_Winter_Classic_Premier_Jersey"
   click_link "Choose Size S"
   find("#addToCart").click
 end
 
 Given(/^NHL \- I apply the Troop ID discount$/) do
-  pending # Write code here that turns the phrase above into concrete actions
+  click_link "militaryAndFirstResponderHeaderTitle"
+  new_window = window_opened_by { find(".desktopIdMeMilitaryBtn").click }
+
+  within_window new_window do
+    sign_in_with_idme
+  end
 end
 
 Given(/^NHL \- I verify the Troop ID discount has been applied$/) do
-  pending # Write code here that turns the phrase above into concrete actions
+  page.has_text? "Status Verified"
 end
