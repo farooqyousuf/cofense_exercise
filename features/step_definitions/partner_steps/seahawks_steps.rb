9da2ad0d69737e7bd2cdf6dbb5871a39d5577d11
Eddie Park <eddie.park@id.me>
2015-11-16 12:59:19 -0500

    CORE-3627: Add seahawks step def outline

---
+++ features/step_definitions/partner_steps/seahawks_steps.rb
@@ -0,0 +1,11 @@
+Given(/^SH \- I add an item to the cart$/) do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Given(/^SH \- I apply the Troop ID discount$/) do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Given(/^SH \- I verify the Troop ID discount has been applied$/) do
+  pending # Write code here that turns the phrase above into concrete actions
+end
