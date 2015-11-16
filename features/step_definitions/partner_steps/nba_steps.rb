f685ce8cf1b238d71eefcdcc358b6cbc07b2bf20
Eddie Park <eddie.park@id.me>
2015-11-16 14:26:07 -0500

    CORE-3624: Update URL to production.yml file

--- features/step_definitions/partner_steps/nba_steps.rb
+++ features/step_definitions/partner_steps/nba_steps.rb
@@ -1,6 +1,6 @@
 
 Given(/^NBA \- I add an item to the cart$/) do
-  visit "http://store.nba.com/Boston_Celtics_Mens/Mens_Boston_Celtics_Larry_Bird_adidas_Kelly_Green_Hardwood_Classics_Swingman_Jersey"
+  visit FigNewton.partners.nba_shop
   if page.has_link?("lightboxSaleCloseLink")
     fanatics_lightbox("NBA")
   end
