2937886660223e17c962c26254115ac2c5499e49
Eddie Park <eddie.park@id.me>
2015-11-16 13:08:32 -0500

    CORE-3627: Add seahawk add item to cart step

--- features/step_definitions/partner_steps/seahawks_steps.rb
+++ features/step_definitions/partner_steps/seahawks_steps.rb
@@ -1,5 +1,8 @@
 Given(/^SH \- I add an item to the cart$/) do
-  pending # Write code here that turns the phrase above into concrete actions
+  visit 'http://proshop.seahawks.com/Seattle_Seahawks_Mens_Jerseys/Mens_Seattle_Seahawks_Russell_Wilson_Nike_College_Navy_Game_Jersey'
+  click_link "Choose Size S"
+  find("#addToCart").click
+  find("#addToCart").click
 end
 
 Given(/^SH \- I apply the Troop ID discount$/) do
