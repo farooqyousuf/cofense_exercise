1782c3722e7e59abf1a56cd190fafe23ad585470
Eddie Park <eddie.park@id.me>
2015-11-16 13:30:16 -0500

    CORE-3627: Add verify troopID

--- features/step_definitions/partner_steps/seahawks_steps.rb
+++ features/step_definitions/partner_steps/seahawks_steps.rb
@@ -6,9 +6,16 @@ Given(/^SH \- I add an item to the cart$/) do
 end
 
 Given(/^SH \- I apply the Troop ID discount$/) do
-  pending # Write code here that turns the phrase above into concrete actions
+  click_link "militaryAndFirstResponderHeaderTitle"
+  new_window = window_opened_by { find(".desktopIdMeMilitaryBtn").click }
+
+  within_window new_window do
+    sign_in_with_idme
+  end
 end
 
 Given(/^SH \- I verify the Troop ID discount has been applied$/) do
-  pending # Write code here that turns the phrase above into concrete actions
+  
+  page.has_text? "A discount of 10% was successfully applied to your order."
+  page.has_text? "Status Verified"
 end
