49296134486700d332cdcc06feaff75739dc299d
Eddie Park <eddie.park@id.me>
2015-11-16 14:00:46 -0500

    CORE-3627: Delete extra click method

--- features/step_definitions/partner_steps/seahawks_steps.rb
+++ features/step_definitions/partner_steps/seahawks_steps.rb
@@ -2,7 +2,6 @@ Given(/^SH \- I add an item to the cart$/) do
   visit "http://proshop.seahawks.com/Seattle_Seahawks_Mens_Jerseys/Mens_Seattle_Seahawks_Russell_Wilson_Nike_College_Navy_Game_Jersey"
   click_link "Choose Size S"
   find("#addToCart").click
-  find("#addToCart").click
 end
 
 Given(/^SH \- I apply the Troop ID discount$/) do
