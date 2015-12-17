53413eab7577e0e71c8bc9cb0c533b995c34f254
Eddie Park <eddie.park@id.me>
2015-12-17 10:34:18 -0500

    CORE-3568: Update NFL mobile specs
    CORE-3568: Add apply TroopID nfl mobile specs with fixes
    
    CORE-3568: Add additional wait period for page load to assert verify ID

--- features/step_definitions/partner_steps/nflmobile_steps.rb
+++ features/step_definitions/partner_steps/nflmobile_steps.rb
@@ -1,20 +1,24 @@
 Given(/^NFLMobile \- I add an item to the cart$/) do
   visit FigNewton.partners.nfl_shop
-
-  if page.has_link? "lightboxSaleCloseLink"
-    click_link "lightboxSaleCloseLink"
-  end
+  page.driver.browser.navigate.refresh
+  sleep 1
   click_button "S"
+  sleep 1
   click_on "mobileAddToCart"
 end
 
 Given(/^NFLMobile \- I apply the Troop ID discount$/) do
+  sleep 1
   find_link("militaryAndFirstResponderHeader").click
+  sleep 1
   find(".idMeMilitaryBtn").click
+
   sign_in_with_idme
+  sleep 3
 end
 
 Given(/^NFLMobile \- I verify the Troop ID discount has been applied$/) do
+  sleep 3
   page.assert_text("Status Verified", :wait => 300)
   page.assert_text("A discount of 15% and free shipping was successfully applied to your order.",:wait => 300)
 end
