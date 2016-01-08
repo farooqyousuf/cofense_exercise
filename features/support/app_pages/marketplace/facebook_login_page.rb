0cb09af26846e94267169c59fb6146ecb575834d
Eddie Park <eddie.park@id.me>
2016-01-07 19:11:03 -0500

    QA-135: Fixed social network login sources

--- features/support/app_pages/marketplace/facebook_login_page.rb
+++ features/support/app_pages/marketplace/facebook_login_page.rb
@@ -2,9 +2,9 @@ class FaceBookLoginPage < IDmeBase
   include IDPBase
 
   def sign_in
+    fill_in_email_by_name(FigNewton.social_network.facebook_email)
+    fill_in_password(FigNewton.social_network.facebook_password)
     binding.pry
-    fill_in_email_by_name(Fignewton.social_network.facebook_email)
-    fill_in_password(Fignewton.social_network.facebook_password)
     click_button("Log In")
   end
 end
