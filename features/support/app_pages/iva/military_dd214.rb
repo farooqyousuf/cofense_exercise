49961a7192c4785a2f681c940f1ecc9aa1babbf3
Farooq <farooqyousuf1@gmail.com>
2016-01-12 15:33:43 -0500

    QA-129: DD214 Legal guardian happy path test complete

--- features/support/app_pages/iva/military_dd214.rb
+++ features/support/app_pages/iva/military_dd214.rb
@@ -19,7 +19,8 @@ class DD214 < IDmeBase
       populate_fields(data_for(:military_dd214))
     end
 
-    if affiliation == "Next of kin deceased veteran"
+    #first and last name for user
+    if ["Next of kin deceased veteran", "Legal guardian"].include?(affiliation)
       find("#first_name").set(data_for(:military_dd214).fetch("first_name"))
       find("#last_name").set(data_for(:military_dd214).fetch("last_name"))
     end
@@ -28,6 +29,7 @@ class DD214 < IDmeBase
   end
 
   def populate_fields(data)
+    #information for service member
   	find("#service_member_first_name").set(data.fetch("member_first_name"))
   	find("#service_member_last_name").set(data.fetch("member_last_name"))
   	find("#social").set(data.fetch("ssn"))
