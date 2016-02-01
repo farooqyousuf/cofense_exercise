6a49d3e172069a40092712531537ad4ff66dbd25
Farooq <farooqyousuf1@gmail.com>
2016-02-01 14:47:57 -0500

    QA-170: removed some comments

--- features/support/app_pages/iva/military_doc_family.rb
+++ features/support/app_pages/iva/military_doc_family.rb
@@ -11,32 +11,3 @@ class MilitaryDocFamily < IDmeBase
   end
 
 end
-
-# [1] pry(#<MilitaryDoc>)> all('.error')[0]['class']
-# => "error"
-# [2] pry(#<MilitaryDoc>)> all('.error')[1]['class']
-# => "error"
-# [3] pry(#<MilitaryDoc>)> all('.error')[2]['class']
-# => "select2-container service-affiliation error"
-# [4] pry(#<MilitaryDoc>)> all('.error')[3]['class']
-# => "select2-offscreen error"
-# [5] pry(#<MilitaryDoc>)> all('.error')[4]['class']
-# => "error"
-# [6] pry(#<MilitaryDoc>)> all('.error')[5]['class']
-# => "error"
-# [7] pry(#<MilitaryDoc>)> all('.error')[6]['class']
-# => "error"
-# [8] pry(#<MilitaryDoc>)> all('.error')[7]['class']
-# => "error"
-# [9] pry(#<MilitaryDoc>)> all('.error')[8]['class']
-# => "error"
-# [10] pry(#<MilitaryDoc>)> all('.error')[9]['class']
-# => "error"
-# [11] pry(#<MilitaryDoc>)> all('.error')[10]['class']
-# => "error"
-# [12] pry(#<MilitaryDoc>)> all('.error')[11]['class']
-# => "select2-container error"
-# [13] pry(#<MilitaryDoc>)> all('.error')[12]['class']
-# => "select2-offscreen error"
-# [14] pry(#<MilitaryDoc>)> all('.error')[13]['class']
-# => "error"
