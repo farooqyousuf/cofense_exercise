c0f272fd376a1fe512ce791a167a3d5bc8dc79c4
Farooq <farooqyousuf1@gmail.com>
2016-02-03 16:41:17 -0500

    cleanup for mil doc step

--- features/step_definitions/iva_steps/military_email_steps.rb
+++ features/step_definitions/iva_steps/military_email_steps.rb
@@ -1,6 +1,6 @@
 Given(/^I verify using military email information for "([^"]*)"$/) do |affiliation|
   @military_email = MilitaryEmail.new
-  @military_email.verify(affiliation, true)
+  @military_email.verify(affiliation)
 end
 
 Given(/^I submit the empty military email form for "([^"]*)"$/) do |group|
