2edf7492673ad3991dd0850e74f56a8046073856
Farooq <farooqyousuf1@gmail.com>
2016-02-03 16:37:13 -0500

    cleanup for dd214 step

--- features/step_definitions/iva_steps/military_dd214_steps.rb
+++ features/step_definitions/iva_steps/military_dd214_steps.rb
@@ -1,7 +1,7 @@
 
 Given(/^I verify using DD214 information for "(.*)"$/) do |affiliation|
   @dd214 = DD214.new
-  @dd214.verify(affiliation, true)
+  @dd214.verify(affiliation)
 end
 
 Given(/^I submit the empty DD214 form for "([^"]*)"$/) do |group|
