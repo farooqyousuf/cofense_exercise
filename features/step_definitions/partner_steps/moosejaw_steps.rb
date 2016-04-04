ba4a61f1c52f511e4b6dcdc00a4a5a483bad434d
Eddie Park <eddie.park@id.me>
2016-04-04 16:54:02 -0400

    QA-178: Add Moosejaw Integration specs

--- features/step_definitions/partner_steps/moosejaw_steps.rb
+++ features/step_definitions/partner_steps/moosejaw_steps.rb
@@ -1,12 +1,27 @@
 
 Given(/^moosejaw \- I add an item to the cart$/) do
-  pending # Write code here that turns the phrase above into concrete actions
+  visit "http://www.moosejaw.com/moosejaw/shop/search_Mens-SS-Shirts____"
+  find(:link, :href=>"http://www.moosejaw.com/moosejaw/shop/product_Arcteryx-Men-s-Captive-SS-Polo_10235054_10208_10000001_-1_").click
+
+  click_link "po-link-selectSize"
+  click_link "pd_prod_Small"
+  find("#productPageAdd2Cart").click
+  find("#MiniShoppingCart").click
+  find("#widget_minishopcart").click
 end
 
 Given(/^moosejaw \- I apply the Troop ID discount$/) do
-  pending # Write code here that turns the phrase above into concrete actions
+  find("#idMe").click
+  idme_window = window_opened_by { find(".login-trigger-military").click  }
+
+  within_window idme_window do
+    sign_in_with_idme
+  end
 end
 
 Given(/^moosejaw \- I verify the troop ID discount has been applied$/) do
-  pending # Write code here that turns the phrase above into concrete actions
+  page.has_text?("IDMEPROMO")
+  page.assert_selector("div.checkout-total-figures",:text =>"$68.95")
+  page.assert_selector("div.checkout-total-figures",:text =>"$13.79")
+  page.assert_selector("div.checkout-total-figures",:text =>"$55.16")
 end
