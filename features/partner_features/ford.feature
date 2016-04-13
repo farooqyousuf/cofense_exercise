7c997105f45eb8f043e6a54c0c4f50de9631c7e8
Eddie Park <eddie.park@id.me>
2016-04-13 19:04:10 -0400

    QA-178: Add Ford Inactive and First Responder specs

--- features/partner_features/ford.feature
+++ features/partner_features/ford.feature
@@ -1,9 +1,12 @@
 @partners @ford @version2
 Feature: Test the Ford Partner Integration
 
-  Background:
-    * ford - I go to the ford military appreciation program
-
   Scenario: Test Ford Military Integration
-    * ford - I register through the Troop ID Integration
-    * ford - I verify the Troop ID Integration for invalid service member
+    * ford - I visit the Military Integration Page
+    * ford - I sign in with IDP
+    * ford - I verify the Troop ID Integration for inactive member
+    @firstresponderford
+  Scenario: Test Ford First Responder Integration
+    * ford - I visit the First Responder Integration Page
+    * ford - I sign in with IDP
+    * ford - I verify the First Responder Integration
--- features/step_definitions/partner_steps/ford_steps.rb
+++ features/step_definitions/partner_steps/ford_steps.rb
@@ -1,8 +1,8 @@
-Given(/^ford \- I go to the ford military appreciation program$/) do
+Given(/^ford \- I visit the Military Integration Page$/) do
   visit "https://www.fordsalutesthosewhoserve.com/"
 end
 
-Given(/^ford \- I register through the Troop ID Integration$/) do
+Given(/^ford \- I sign in with IDP$/) do
   new_window = window_opened_by { find(".register").click }
 
   within_window new_window do
@@ -10,13 +10,24 @@ Given(/^ford \- I register through the Troop ID Integration$/) do
   end
 end
 
-Given(/^ford \- I verify the Troop ID Integration for invalid service member$/) do
+Given(/^ford \- I verify the Troop ID Integration for inactive member$/) do
   page.has_text? "Thank you for your service! We see you are a Veteran but need a little more information in order for you to receive the offer. Did you leave active duty after September 30, 1985?"
 
   click_on "Yes"
 
   verify_idme = page.driver.browser.window_handles.last
   page.driver.browser.switch_to.window(verify_idme)
-
+  sleep 2
   expect(page.driver.browser.current_url).to have_text "https://verify.id.me/military/"
+end
+
+Given(/^ford \- I visit the First Responder Integration Page$/) do
+ visit "https://www.fordspecialoffer.com/firstresponders"
+end
+
+Given(/^ford \- I verify the First Responder Integration$/) do
+ select("Ford",:from =>"lstIntBrand")
+
+ expect(page.driver.browser.current_url).to have_text "https://www.fordspecialoffer.com/firstresponders/Register/EnrollFord"
+ page.has_text? "First Responder Appreciation Bonus Cash"
 end
