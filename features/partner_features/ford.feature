7ba6d6235009dc9ce300662f6f40f0ffd239459f
Eddie Park <eddie.park@id.me>
2016-04-14 12:48:30 -0400

    QA-178: Add ford active service member path

--- features/partner_features/ford.feature
+++ features/partner_features/ford.feature
@@ -1,12 +1,17 @@
 @partners @ford @version2
 Feature: Test the Ford Partner Integration
 
-  Scenario: Test Ford Military Integration
+  Scenario: Test Ford Military Integration for inactive service member
     * ford - I visit the Military Integration Page
     * ford - I sign in with IDP
     * ford - I verify the Troop ID Integration for inactive member
-    @firstresponderford
+
   Scenario: Test Ford First Responder Integration
     * ford - I visit the First Responder Integration Page
     * ford - I sign in with IDP
     * ford - I verify the First Responder Integration
+  @active_mem
+  Scenario: Test Ford Military for active service member
+    * ford - I visit the Military Integration Page
+    * ford - I sign in with IDP as active member
+    * ford - I verify the Troop ID Integration for active member
--- features/step_definitions/partner_steps/ford_steps.rb
+++ features/step_definitions/partner_steps/ford_steps.rb
@@ -31,3 +31,32 @@ Given(/^ford \- I verify the First Responder Integration$/) do
  expect(page.driver.browser.current_url).to have_text "https://www.fordspecialoffer.com/firstresponders/Register/EnrollFord"
  page.has_text? "First Responder Appreciation Bonus Cash"
 end
+
+Given(/^ford \- I sign in with IDP as active member$/) do
+  new_window = window_opened_by { find(".register").click }
+
+  within_window new_window do
+    sign_in_with_idme_active_service_member
+  end
+end
+
+Given(/^ford \- I verify the Troop ID Integration for active member$/) do
+  select("Ford",:from =>"lstIntBrand")
+
+  expect(page.driver.browser.current_url).to have_text "https://www.fordsalutesthosewhoserve.com/Register/EnrollFord"
+  page.has_text? "Military Appreciation Bonus Cash"
+end
+
+def sign_in_with_idme_active_service_member
+  if page.has_content? "Sign in"
+    fill_in "user_email", :with => FigNewton.partners.ford_user
+    fill_in "user_password", :with => FigNewton.partners.ford_password
+    click_button "Sign in"
+  end
+
+  if page.has_content? "Click here to continue"
+    click_link "Click here to continue"
+  elsif page.has_content? "Authorize"
+    click_button "Authorize"
+  end
+end
