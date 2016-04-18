a5b8f9e59edf95b2cc0db451813ab96ab594a1b2
Eddie Park <eddie.park@id.me>
2016-04-18 17:32:33 -0400

    QA-201:Add step def outline

---
+++ features/step_definitions/partner_steps/taylor_made_steps.rb
@@ -0,0 +1,11 @@
+Given(/^TAYLOR MADE \- I add a item to the checkout cart$/) do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Given(/^TAYLOR MADE \- I apply the Troop ID discount$/) do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Given(/^TAYLOR MADE \- I verify the Troop ID discount has been applied$/) do
+  pending # Write code here that turns the phrase above into concrete actions
+end
