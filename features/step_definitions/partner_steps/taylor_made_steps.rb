b30d398295e2c55aa1ccd03aaa45c70ea62619f7
Eddie Park <eddie.park@id.me>
2016-04-19 16:03:43 -0400

    QA-201: Correct spacing with specs

--- features/step_definitions/partner_steps/taylor_made_steps.rb
+++ features/step_definitions/partner_steps/taylor_made_steps.rb
@@ -2,20 +2,20 @@ Given(/^TAYLOR MADE \- I add a item to the checkout cart$/) do
   visit FigNewton.partners.taylormadegolf
   page.driver.browser.navigate.refresh
   click_button "Add to Cart"
-	first(:link ,:href =>"https://taylormadegolf.com/cart").click
+  first(:link ,:href =>"https://taylormadegolf.com/cart").click
 end
 
 Given(/^TAYLOR MADE \- I apply the Troop ID discount$/) do
-
-	page.execute_script "window.scrollBy(0.,300)"
- 	new_window = window_opened_by { click_link "Troop ID"} 
+  page.execute_script "window.scrollBy(0.,300)"
+  new_window = window_opened_by { click_link "Troop ID" } 
+ 
   within_window new_window do
     sign_in_with_idme
   end
 end
 
 Given(/^TAYLOR MADE \- I verify the Troop ID discount has been applied$/) do
-	page.has_text? "You Saved: - $75.00"
-	page.has_text? "- ID.me Discount 15% off"
-	page.has_text? "Order Total: $424.99"
+  page.has_text? "You Saved: - $75.00"
+  page.has_text? "- ID.me Discount 15% off"
+  page.has_text? "Order Total: $424.99"
 end
