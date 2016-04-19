e38221189a0f676c63de4aa6344f9aa59c807114
Eddie Park <eddie.park@id.me>
2016-04-19 15:41:50 -0400

    QA-201: Add Taylormadegolf.com specs

--- features/step_definitions/partner_steps/taylor_made_steps.rb
+++ features/step_definitions/partner_steps/taylor_made_steps.rb
@@ -1,11 +1,21 @@
 Given(/^TAYLOR MADE \- I add a item to the checkout cart$/) do
-  pending # Write code here that turns the phrase above into concrete actions
+  visit "http://taylormadegolf.com/M1-Driver/N1184507.html"
+  page.driver.browser.navigate.refresh
+  click_button "Add to Cart"
+	first(:link ,:href =>"https://taylormadegolf.com/cart").click
 end
 
 Given(/^TAYLOR MADE \- I apply the Troop ID discount$/) do
-  pending # Write code here that turns the phrase above into concrete actions
+
+	page.execute_script "window.scrollBy(0.,300)"
+ 	new_window = window_opened_by { click_link "Troop ID"} 
+  within_window new_window do
+    sign_in_with_idme
+  end
 end
 
 Given(/^TAYLOR MADE \- I verify the Troop ID discount has been applied$/) do
-  pending # Write code here that turns the phrase above into concrete actions
+	page.has_text? "You Saved: - $75.00"
+	page.has_text? "- ID.me Discount 15% off"
+	page.has_text? "Order Total: $424.99"
 end
