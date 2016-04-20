4f42a3299612b2a9840c7594cc4aa98beb8374a9
Eddie Park <eddie.park@id.me>
2016-04-20 16:15:34 -0400

    QA-201: Add prelimary steps for Adidas Golf partner

--- features/step_definitions/partner_steps/taylor_made_steps.rb
+++ features/step_definitions/partner_steps/taylor_made_steps.rb
@@ -21,13 +21,27 @@ Given(/^TAYLOR MADE \- I verify the Troop ID discount has been applied$/) do
 end
 
 Given(/^ADIDAS GOLF \- I add a item to the checkout cart$/) do
-  pending # Write code here that turns the phrase above into concrete actions
+  visit FigNewton.partners.adidasgolf
+  page.driver.browser.navigate.refresh
+  sleep 1
+  page.execute_script("document.getElementById('va-tm_comfil_size').style.display='block'") #select box is hidden by default
+  select "Small", :from => "va-tm_comfil_size"
+
+  page.execute_script("window.scrollBy(0,-400)")
+  sleep 1
+  find("#add-to-cart").click
+  first(:link, :href => "https://adidasgolf.com/cart").click 
 end
 
 Given(/^ADIDAS GOLF \- I apply the Troop ID discount$/) do
-  pending # Write code here that turns the phrase above into concrete actions
+  page.execute_script "window.scrollBy(0.,300)"
+  new_window = window_opened_by { click_link "Troop ID" } 
+ 
+  within_window new_window do
+    sign_in_with_idme
+  end
 end
 
-Given(/^ADIDAS GOLF \- I verify the Troop ID discount has been applied$/) do
-  pending # Write code here that turns the phrase above into concrete actions
+Given(/^ADIDAS GOLF \- I verify the Troop ID discount has been applied$/) do  
+  page.has_text? "- ID.me Discount 15% off"
 end
