8ee56477ec756b1dbe2982abaff97ae945bfbf79
Eddie Park <eddie.park@id.me>
2016-04-20 08:34:51 -0400

    QA-201: Add adidas golf feature and step outline

--- features/step_definitions/partner_steps/taylor_made_steps.rb
+++ features/step_definitions/partner_steps/taylor_made_steps.rb
@@ -19,3 +19,15 @@ Given(/^TAYLOR MADE \- I verify the Troop ID discount has been applied$/) do
   page.has_text? "- ID.me Discount 15% off"
   page.has_text? "Order Total: $424.99"
 end
+
+Given(/^ADIDAS GOLF \- I add a item to the checkout cart$/) do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Given(/^ADIDAS GOLF \- I apply the Troop ID discount$/) do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Given(/^ADIDAS GOLF \- I verify the Troop ID discount has been applied$/) do
+  pending # Write code here that turns the phrase above into concrete actions
+end
