f6ec85e63924f485944376dec420eede88988e87
Farooq <farooqyousuf1@gmail.com>
2016-04-21 12:09:36 -0400

    minor changes

--- features/step_definitions/idp_steps/idp_signin_steps.rb
+++ features/step_definitions/idp_steps/idp_signin_steps.rb
@@ -17,7 +17,6 @@ Given(/^I login as a "([^"]*)" user$/) do |user_type|
   @idp_signin.sign_in(user, password)
 end
 
-
 Given(/^I should be successfully authenticated(?: using "(.*)")?$/) do |method|
   @oauth_client.save_token(current_url)
 
@@ -33,20 +32,6 @@ Given(/^I should be successfully authenticated(?: using "(.*)")?$/) do |method|
   end
 
   expect(@oauth_client.verified?).to eq(true)
-end
-
-Given(/^I should be successfully verified(?: as "(.*)")?$/) do |group|
-  flag = ["LOA1", "LOA2", "LOA3"].include?(group)
-
-  #save oauth client token for idp and iva tests
-  @oauth_client.save_token(current_url)
-
-  if flag == true
-    expect(@oauth_client.verify_loa_scope(group)).to eq(true)
-  else
-    expect(@oauth_client.verified?).to eq(true)
-    expect(@oauth_client.has_affiliation?(group)).to eq(true) if group
-  end
 end
 
 Given(/^I login with an invalid password$/) do
