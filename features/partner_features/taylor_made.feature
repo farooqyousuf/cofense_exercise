aeba15011126d5be994c745557564fb5d865dcd4
Eddie Park <eddie.park@id.me>
2016-04-25 11:18:40 -0400

    QA-201: Add ashworth golf partner spec and fix rounding error with discount method

--- features/partner_features/taylor_made.feature
+++ features/partner_features/taylor_made.feature
@@ -11,3 +11,15 @@ Feature: Test Taylor Made partner sites integrations
     * ADIDAS GOLF - I add a item to the checkout cart
     * ADIDAS GOLF - I apply the Troop ID discount
     * ADIDAS GOLF - I verify the Troop ID discount has been applied 
+
+  @ashworthgolf
+  Scenario: Test military integration for ashworthgolf.com
+    * ASHWORTH GOLF - I add a item to the checkout cart
+    * ASHWORTH GOLF - I apply the Troop ID discount
+    * ASHWORTH GOLF - I verify the Troop ID discount has been applied 
+
+  @adamsgolf
+  Scenario: Test military integration for adamsgolf.com 
+    * ADAMS GOLF - I add a item to the checkout cart
+    * ADAMS GOLF - I apply the Troop ID discount
+    * ADAMS GOLF - I verify the Troop ID  discount has been applied 
--- features/step_definitions/partner_steps/taylor_made_steps.rb
+++ features/step_definitions/partner_steps/taylor_made_steps.rb
@@ -57,6 +57,39 @@ Given(/^ADIDAS GOLF \- I verify the Troop ID discount has been applied$/) do
 end
 
 
+Given(/^ASHWORTH GOLF \- I add a item to the checkout cart$/) do
+  visit FigNewton.partners.ashworthgolf
+  page.driver.browser.navigate.refresh
+  sleep 1
+  page.execute_script("document.getElementById('va-tm_comfil_size').style.display='block'") #select box is hidden by default
+  select "Small", :from => "va-tm_comfil_size"
+
+  page.execute_script("window.scrollBy(0,-400)")
+  sleep 1
+  find("#add-to-cart").click
+  first(:link, :href => "https://ashworthgolf.com/cart").click 
+end
+
+Given(/^ASHWORTH GOLF \- I apply the Troop ID discount$/) do
+    page.execute_script "window.scrollBy(0.,300)"
+  new_window = window_opened_by { click_link "Troop ID" } 
+ 
+  within_window new_window do
+    sign_in_with_idme
+  end
+end
+
+Given(/^ASHWORTH GOLF \- I verify the Troop ID discount has been applied$/) do
+  original_product_amt_string = find(".price-unadjusted").text
+  actual_product_discounted_amt_string = find(".total-savings").find("span:nth-child(2)").text 
+
+  #TODO: replace ".15" with data_for
+  discount_applied = verify_discount(original_product_amt_string, actual_product_discounted_amt_string, ".15") 
+  expect(discount_applied).to be(true)
+  page.has_text? "- ID.me Discount 15% off"
+end
+
+
 
 
 
