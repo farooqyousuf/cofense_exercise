54e0c2a85d4c2e258a569bd3e8c080e530abdb4c
Eddie Park <eddie.park@id.me>
2016-04-27 18:28:49 -0400

    QA-201: Remove pry

--- features/support/test_helper.rb
+++ features/support/test_helper.rb
@@ -72,7 +72,6 @@ module HelperMethods
    full_price = original_product_amt_string.delete "$"
    calc_discount_amt = ( full_price.to_f * discount_percentage.to_f ).to_i
    actual_discount_amt = /\d{1,3}[,\\.]?(\\d{1,2})?/.match(actual_product_discounted_amt_string)[0].chop.to_i 
-   binding.pry
    return actual_discount_amt == calc_discount_amt 
   end 
 
