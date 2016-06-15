a96366fb777561b591ac068c75f9ef629ae823cc
Farooq <farooqyousuf1@gmail.com>
2016-06-15 17:11:08 -0400

    QA-264: Denied attempt test for emt national

--- features/step_definitions/iva_steps/emt_national_steps.rb
+++ features/step_definitions/iva_steps/emt_national_steps.rb
@@ -1,7 +1,11 @@
 Given(/^I verify using nationally certified EMT credentials$/) do
-  NationalEMT.new.verify
+  NationalEMT.new.verify(type: "unique")
 end
 
 Given(/^I submit the empty National EMT form$/) do
-  NationalEMT.new.verify(false)
+  NationalEMT.new.verify(populate: false)
+end
+
+Given(/^I submit the emt national verification form as a "([^"]*)" record$/) do |type|
+  NationalEMT.new.verify(type: type)
 end
