92bb21d8d0f72f86b38f3711818a1d35de6ce104
Eddie Park <eddie.park@id.me>
2016-06-20 11:46:51 -0400

    QA-277: Add group affiliations page object and initializer

--- features/step_definitions/marketplace/mp_user_group_affiliations_steps.rb
+++ features/step_definitions/marketplace/mp_user_group_affiliations_steps.rb
@@ -1,8 +1,11 @@
 Given(/^I sign-up with a generic test user account$/) do
 	@marketplace_shop = MarketplaceLandingPAge.new
-	@marketplace_shop.sign_up
+	@marketplace_shop.sign_up(FigNewton.user.test_email,FigNewton.user.test_email)
 end
 
 Given(/^I select to verify the Military Group Affiliation$/) do
 	@marketplace_group_affiliations = MarketplaceGroupAffiliationPage.new 
+	@marketplace_group_affiliations.verify_military_affiliation 
+
+	#expectation to verify? set as steps? 
 end
---
+++ features/support/app_pages/marketplace/mp_group_affiliations_page.rb
@@ -0,0 +1,9 @@
+class MarketplaceGroupAffiliationsPage < IDmeBase
+	include IDPBase
+	include RSpec::Matchers
+
+	def initialize 
+		super(FigNewton.marketplace.group_affiliations_page)
+	end
+
+end 
