5b257a757484825dfdedca2a30ae36eaf8501f87
Farooq <farooqyousuf1@gmail.com>
2016-07-01 09:48:22 -0400

    HOTFIX: Fixed idp unlock tests

--- features/step_definitions/idp_steps/idp_unlock_steps.rb
+++ features/step_definitions/idp_steps/idp_unlock_steps.rb
@@ -1,7 +1,7 @@
 Given(/^I lockout my account$/) do
   @idp_unlock = IDPUnlock.new
   @idp_unlock.lockout_account(@username)
-  step 'I should see the red alert box error message "Your account has been locked due to a number of failed sign-in attempts. Please click here to unlock it."'
+  step 'I should see the red alert box error message "Your ID.me Wallet has been locked due to a high number of failed sign-in attempts. Please click here to unlock it."'
 end
 
 Given(/^I click on the unlock account link$/) do
