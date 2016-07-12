c42f8683502e0bc2bf714e52b424d78701906b1f
Eddie Park <eddie.park@id.me>
2016-07-12 10:47:02 -0400

    QA-304: Add marketplace offer index step defs

---
+++ features/step_definitions/marketplace/mp_offer_steps.rb
@@ -0,0 +1,23 @@
+Given(/^I create a Marketplace Offers index page object$/) do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Given(/^I expect the page url to be for Marketplace Offers Page$/) do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Given(/^I check that the Offers Index is available$/) do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Given(/^I check that store offer index card is visible$/) do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Given(/^I check the store offer card links for "([^"]*)" User$/) do |arg1|
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Given(/^I check that the store offer card favorite and report bug is no visible$/) do
+  pending # Write code here that turns the phrase above into concrete actions
+end
