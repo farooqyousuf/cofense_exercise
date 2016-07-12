71f62a922079051b404cdf3817902363cbf74d21
Eddie Park <eddie.park@id.me>
2016-07-12 15:51:33 -0400

    QA-304: Add Offer specs for Logged out User

--- features/marketplace_users_features/mp_offers_page.feature
+++ features/marketplace_users_features/mp_offers_page.feature
@@ -3,13 +3,14 @@
 Feature: Test Marketplace Offers Page 
 	Background:
 		* I visit "MarketplaceLandingPage"
-		* I create a Marketplace Offers index page object 
+		* I create a Marketplace Offers index page object
+		* I visit "MarketplaceOffersPage"
 
 	Scenario: Check Offers Index Table - Logged Out User
 		* I expect the page url to be for Marketplace Offers Page
 		* I check that the Offers Index is available
 		* I check that store offer index card is visible
 		* I check the store offer card links for "Logged Out" User
-		* I check that the store offer card favorite and report bug is no visible 
+		* I check that the store offer card favorite and report bug is not visible 
 		
 
--- features/step_definitions/marketplace/mp_offer_steps.rb
+++ features/step_definitions/marketplace/mp_offer_steps.rb
@@ -1,23 +1,28 @@
 Given(/^I create a Marketplace Offers index page object$/) do
-  pending # Write code here that turns the phrase above into concrete actions
+	@marketplace_offers = MarketplaceOffersPage.new 
 end
 
 Given(/^I expect the page url to be for Marketplace Offers Page$/) do
-  pending # Write code here that turns the phrase above into concrete actions
+	expect(page.current_url).to eql(FigNewton.marketplace.offers_index_page)
 end
 
 Given(/^I check that the Offers Index is available$/) do
-  pending # Write code here that turns the phrase above into concrete actions
+	expect(page).to have_css(".offers-list-static")
+	expect(page).to have_css(".listing__header .heading .breadcrumbs", :text =>"Shop › Offers")
 end
 
 Given(/^I check that store offer index card is visible$/) do
-  pending # Write code here that turns the phrase above into concrete actions
+	expect(page).to have_css(".offers-list-static .resource-card",:match => :first) 
 end
 
-Given(/^I check the store offer card links for "([^"]*)" User$/) do |arg1|
-  pending # Write code here that turns the phrase above into concrete actions
+Given(/^I check the store offer card links for "([^"]*)" User$/) do |user_type|
+	if "Logged Out"
+		expect(page).to have_css(".resource-card .button", :match => :first , :text =>"Sign Up & Save")
+		expect(page).to have_css(".resource-card .offer-card__share-alt",:match => :first , :text =>"Share with Friends")
+	end 
 end
 
-Given(/^I check that the store offer card favorite and report bug is no visible$/) do
-  pending # Write code here that turns the phrase above into concrete actions
+Given(/^I check that the store offer card favorite and report bug is not visible$/) do
+	expect(page).to have_no_selector(".save-offer")
+	expect(page).to have_no_selector(".report-offer")
 end
