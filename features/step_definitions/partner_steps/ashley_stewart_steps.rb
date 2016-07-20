d7ea98a6eb69cfbe08af092b73a3a041707a9aad
Eddie Park <eddie.park@id.me>
2016-07-20 09:48:24 -0400

    QA-312: Add Ashley Stewart teacher integration test

--- features/step_definitions/partner_steps/ashley_stewart_steps.rb
+++ features/step_definitions/partner_steps/ashley_stewart_steps.rb
@@ -12,13 +12,10 @@ Given(/^ASHLEY\- I visit the checkout page$/) do
 	page.driver.browser.navigate.refresh # to remove pop-up promotion
 end
 
-Given(/^ASHLEY\- I sign in with my Id\.me military$/) do
-
+Given(/^ASHLEY\- I sign in with my Id\.me military account$/) do
 	find(".idme-buttons-wrapper").click 
 	idp_signin = window_opened_by do
-   
    	find(".IDmeButtonMil a").click
-
   end
 
   within_window idp_signin do
@@ -26,8 +23,29 @@ Given(/^ASHLEY\- I sign in with my Id\.me military$/) do
   end
 end
 
-Given(/^ASHLEY\- I verify my military discounted item$/) do
+Given(/^ASHLEY\- I verify my military discount was applied$/) do
 	expect(page).to have_css(".cart-promo",:text => "Order Discount: Extra 10% Discount for our Military! Thanks for all you do. ....... Ashley")
+
+	original_product_amt_string = find(".price-adjusted-total").text
+	actual_product_discount_amt_string = find(".order-discount td:nth-child(2)").text
+
+	discount_applied = verify_discount(original_product_amt_string, actual_product_discount_amt_string, ".10")
+	expect(discount_applied).to be(true)
+end
+
+Given(/^ASHLEY\- I sign in with my Id\.me teacher account$/) do
+	find(".idme-buttons-wrapper").click 
+	idp_signin = window_opened_by do
+   	find(".IDmeButtonTea a").click
+  end
+
+  within_window idp_signin do
+    sign_in_with_idme
+  end
+end
+
+Given(/^ASHLEY\- I verify my teacher discount was applied$/) do
+	expect(page).to have_css(".cart-promo",:text => "Order Discount: Extra 10% Discount for our Teachers! Thanks for all you do. ....... Ashley")
 
 	original_product_amt_string = find(".price-adjusted-total").text
 	actual_product_discount_amt_string = find(".order-discount td:nth-child(2)").text
