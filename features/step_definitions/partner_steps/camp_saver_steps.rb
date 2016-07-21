419bfa83053e331d95942714061ba0e7a791b822
Eddie Park <eddie.park@id.me>
2016-07-21 09:04:35 -0400

    QA-313: Add item to product cart , checkout and sign-in with idme troop

--- features/step_definitions/partner_steps/camp_saver_steps.rb
+++ features/step_definitions/partner_steps/camp_saver_steps.rb
@@ -4,19 +4,26 @@ Given(/^CAMP\- I check the Camp Saver ID\.me Verify Instruction Page$/) do
 end
 
 Given(/^CAMP\- I click Shop now and verify redirect to camp saver partner$/) do
-  pending # Write code here that turns the phrase above into concrete actions
+	find(:link,:href=>"/").click 
+	expect(page.current_url).to eql("http://www.campsaver.com/")
 end
 
 Given(/^CAMP\- I visit the product page$/) do
-  pending # Write code here that turns the phrase above into concrete actions
+	visit FigNewton.partners.camp_saver.product_page 
 end
 
 Given(/^CAMP\- I add product to the cart and checkout$/) do
-  pending # Write code here that turns the phrase above into concrete actions
+	select "Seafoam - Large: $39.95",:from => "super_attributes_single_select"
+	find(".btn-cart").click
+	find(".checkout").click 
 end
 
 Given(/^CAMP\- I apply the Troop ID discount$/) do
-  pending # Write code here that turns the phrase above into concrete actions
+  idp_signin = window_opened_by { find(".idme-connect-trigger").click }
+
+  within_window idp_signin do
+    sign_in_with_idme
+  end
 end
 
 Given(/^CAMP\- I verify the Troop ID discount has been applied$/) do
