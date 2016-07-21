c65c3f0783cdb7899d33734b55494c5d6e87010f
Eddie Park <eddie.park@id.me>
2016-07-21 10:10:57 -0400

    QA-313: Verify TroopID discount applied

--- features/step_definitions/partner_steps/camp_saver_steps.rb
+++ features/step_definitions/partner_steps/camp_saver_steps.rb
@@ -1,6 +1,9 @@
 Given(/^CAMP\- I check the Camp Saver ID\.me Verify Instruction Page$/) do
 	visit FigNewton.partners.camp_saver.verify_idme_page
 	expect(page).to have_css("h2", :text =>"CampSaver.com Offers 20% Off Regular Price For Military, First Responders & Students")
+	#having issue with a delayed modal pop=up maybe like a delayed refresh?
+	sleep 2
+	page.driver.browser.navigate.refresh
 end
 
 Given(/^CAMP\- I click Shop now and verify redirect to camp saver partner$/) do
@@ -19,7 +22,7 @@ Given(/^CAMP\- I add product to the cart and checkout$/) do
 end
 
 Given(/^CAMP\- I apply the Troop ID discount$/) do
-  idp_signin = window_opened_by { find(".idme-connect-trigger").click }
+  idp_signin = window_opened_by { find(".idme-btn-primary-lg-Troop").click }
 
   within_window idp_signin do
     sign_in_with_idme
@@ -27,5 +30,10 @@ Given(/^CAMP\- I apply the Troop ID discount$/) do
 end
 
 Given(/^CAMP\- I verify the Troop ID discount has been applied$/) do
-  pending # Write code here that turns the phrase above into concrete actions
+	expect(page).to have_css(".success-msg",:text =>"Successfully verified your affiliation via ID.me")
+	expect(page).to have_css("#shopping-cart-table tbody td:nth-child(2) div" ,:text =>"ID.me Group Discount Applied") 
+	original_prudct_amt_string = first("#shopping-cart-table .price_old").text
+	actual_product_discount_amt_string = find("#shopping-cart-totals-table tbody tr:nth-child(3) td:nth-child(2)").text 
+	discount_applied = verify_discount(original_prudct_amt_string,actual_product_discount_amt_string,".20", :exact_match => true )
+	expect(discount_applied).to be(true)
 end
