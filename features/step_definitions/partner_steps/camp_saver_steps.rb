95e644e3c08c9b2cc5a253020f41b5813a795b10
Eddie Park <eddie.park@id.me>
2016-07-20 23:01:16 -0400

    QA-313: Check campsaver verify with idme page

---
+++ features/step_definitions/partner_steps/camp_saver_steps.rb
@@ -0,0 +1,24 @@
+Given(/^CAMP\- I check the Camp Saver ID\.me Verify Instruction Page$/) do
+	visit FigNewton.partners.camp_saver.verify_idme_page
+	expect(page).to have_css("h2", :text =>"CampSaver.com Offers 20% Off Regular Price For Military, First Responders & Students")
+end
+
+Given(/^CAMP\- I click Shop now and verify redirect to camp saver partner$/) do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Given(/^CAMP\- I visit the product page$/) do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Given(/^CAMP\- I add product to the cart and checkout$/) do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Given(/^CAMP\- I apply the Troop ID discount$/) do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Given(/^CAMP\- I verify the Troop ID discount has been applied$/) do
+  pending # Write code here that turns the phrase above into concrete actions
+end
