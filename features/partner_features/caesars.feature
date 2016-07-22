303ac1c7277e088db95618a6efc2ab3ed21bbd5e
Eddie Park <eddie.park@id.me>
2016-07-22 11:22:27 -0400

    QA-314: Refactor caesars step defs for multiple user groups

--- features/partner_features/caesars.feature
+++ features/partner_features/caesars.feature
@@ -3,7 +3,9 @@ Feature: Test the ID.me Caesars Integration
 
   Scenario: Test the Caesars Military Partner Integration
     * CAESARS- I visit homepage and check best room rates
-    * CAESARS- I verify my military account through ID.me 
-    * CAESARS- I verify my military integration is displayed on the rate calendar page 
-    * CAESARS- I choose a room date and verify the military discount on the modal
-    * CAESARS- I book a room and verify my military discount has been applied 
+    * CAESARS- I verify my "troop_id" through ID.me
+    * CAESARS- I verify my "troop_id" integration is displayed on the rate calendar page
+    * CAESARS- I choose a room date and verify the "troop_id" discount on the modal
+    * CAESARS- I book a room and verify my "troop_id" discount has been applied
+
+
--- features/step_definitions/partner_steps/caesars_steps.rb
+++ features/step_definitions/partner_steps/caesars_steps.rb
@@ -5,31 +5,75 @@ Given(/^CAESARS\- I visit homepage and check best room rates$/) do
   click_link "Check Best Rates"
 end
 
-Given(/^CAESARS\- I verify my military account through ID\.me$/) do
-  @idp_signin = window_opened_by do
-    find(".idme-btn-primary-sm-Troop-main").click
-  end
+Given(/^CAESARS\- I verify my "([^"]*)" through ID\.me$/) do |group|
+  user_group = case group
+    when "troop_id"
+      ".idme-btn-primary-sm-Troop-main"
+    when "teacher_id"
+      ".idme-btn-primary-sm-Teacher"
+    when "responder_id"
+      ".idme-btn-primary-sm-Responder"
+    when "student_id"
+      ".idme-btn-primary-sm-Student"
+  end 
 
-  within_window @idp_signin do
-    sign_in_with_idme_account("troop_id")
-  end
+  @idp_signin = window_opened_by do
+    find(user_group).click 
+  end 
+
+  within_window @idp_signin do 
+    sign_in_with_idme_account(group)
+  end 
 end
 
-Given(/^CAESARS\- I verify my military integration is displayed on the rate calendar page$/) do
-  expect(page).to have_css(".id-me-bx",:text =>"Additional Military Discount applied through")
+Given(/^CAESARS\- I verify my "([^"]*)" integration is displayed on the rate calendar page$/) do |group|
+   user_group_text = case group
+     when "troop_id"
+      "Military"
+     when "teacher_id"
+      "Teacher"
+     when "responder_id"
+      "First Responder"
+     when "student_id"
+      "Student"
+   end 
+
+  expect(page).to have_css(".id-me-bx",:text =>"Additional #{user_group_text} Discount applied through")
   expect(page).to have_css(".troopSwapSidepanel",:text =>"ID.me Up to 15% Room Discount for Military, First Responders, Students & Teachers. What is ID.me? Verification by ID.me")
 end
 
-Given(/^CAESARS\- I choose a room date and verify the military discount on the modal$/) do
+Given(/^CAESARS\- I choose a room date and verify the "([^"]*)" discount on the modal$/) do |group|
+  user_group_text = case group
+     when "troop_id"
+      "Military"
+     when "teacher_id"
+      "Teacher"
+     when "responder_id"
+      "First Responder"
+     when "student_id"
+      "Student"
+  end 
+
   find(".cal-table div:nth-child(2) div:nth-child(3)").click 
-  expect(page).to have_css("#idme",:text =>"Additional Military Discount applied through ID.me")
+  expect(page).to have_css("#idme",:text =>"Additional #{user_group_text} Discount applied through ID.me")
   
   find(".rate-table dl:nth-child(1) .cfive").click 
   expect(page).to have_css(".troopSwapSidepanel",:text =>"ID.me Up to 15% Room Discount for Military, First Responders, Students & Teachers. What is ID.me? Verification by ID.me")
 end
 
-Given(/^CAESARS\- I book a room and verify my military discount has been applied$/) do
+Given(/^CAESARS\- I book a room and verify my "([^"]*)" discount has been applied$/) do |group|
+  user_group_text = case group
+     when "troop_id"
+      "Military"
+     when "teacher_id"
+      "Teacher"
+     when "responder_id"
+      "First Responder"
+     when "student_id"
+      "Student"
+  end 
+
   first(".hotel-list #A1").click 
-  expect(page).to have_css(".title dd:nth-child(3)",:text => "Military Discount: 10% - Applied")
+  expect(page).to have_css(".title dd:nth-child(3)",:text => "#{user_group_text} Discount: 10% - Applied")
   #NOTE: Caesears doesn't disclose their promotional discount amounts so cannot at this time confirm discount being applied
 end
