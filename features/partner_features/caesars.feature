ee11b36c9749bff25a2c9848f302d64328c70df9
Eddie Park <eddie.park@id.me>
2016-07-22 09:54:10 -0400

    QA-314: Add Caesars military step def integration test

--- features/partner_features/caesars.feature
+++ features/partner_features/caesars.feature
@@ -1,7 +1,9 @@
 @caesars @partners @desktop
 Feature: Test the ID.me Caesars Integration
-  @troop_id
+
   Scenario: Test the Caesars Military Partner Integration
-    * CS - I add an item to the cart
-    * CS - I apply the Troop ID discount
-    * CS - I verify the discount has been applied
+    * CAESARS- I visit homepage and check best room rates
+    * CAESARS- I verify my military account through ID.me 
+    * CAESARS- I verify my military integration is displayed on the rate calendar page 
+    * CAESARS- I choose a room date and verify the military discount on the modal
+    * CAESARS- I book a room and verify my military discount has been applied 
--- features/step_definitions/partner_steps/caesars_steps.rb
+++ features/step_definitions/partner_steps/caesars_steps.rb
@@ -1,29 +1,35 @@
-Given(/^CS \- I add an item to the cart$/) do
+Given(/^CAESARS\- I visit homepage and check best room rates$/) do
   visit FigNewton.partners.caesars_hotels
+  find(".cet-dialog-close-box").click #for pop-up modal upon visit 
+
   click_link "Check Best Rates"
-  # this is UP TO 15% which means it'll be different which means that you are going to need
-  #to make sure they're variable
-  find("#datalist").first("a",:text => "ROOMS & RATES").click
 end
 
-Given(/^CS \- I apply the Troop ID discount$/) do
+Given(/^CAESARS\- I verify my military account through ID\.me$/) do
   @idp_signin = window_opened_by do
-    first(".idme-btn-container a").native.send_key :return
+    find(".idme-btn-primary-sm-Troop-main").click
   end
 
   within_window @idp_signin do
-    sign_in_with_idme
+    sign_in_with_idme_account("troop_id")
   end
-  # note that will need sleep or extended sync selector because visible delay in content loading
-  # TODO : add expectations to check first count that discount has been applied here
-  expect(page).to have_css(".idmeimg") # Military Discount Applied image
-  binding.pry
-  expect(find("#roomList").first(".box").find(".troop-promo").text).to eql("+Additional Military Discount")
-  find("#roomList").first("a",:text => "BOOK NOW").click
 end
 
-Given(/^CS \- I verify the discount has been applied$/) do
-  expect(find(".title")).to have_text("Military Discount: 10%")
-  #I dont think there is a good way to find the price amount to be set right now?
-  #TODO : turns out the new window was incorrect so instead adding expectations to check otherwise
+Given(/^CAESARS\- I verify my military integration is displayed on the rate calendar page$/) do
+  expect(page).to have_css(".id-me-bx",:text =>"Additional Military Discount applied through")
+  expect(page).to have_css(".troopSwapSidepanel",:text =>"ID.me Up to 15% Room Discount for Military, First Responders, Students & Teachers. What is ID.me? Verification by ID.me")
+end
+
+Given(/^CAESARS\- I choose a room date and verify the military discount on the modal$/) do
+  find(".cal-table div:nth-child(2) div:nth-child(3)").click 
+  expect(page).to have_css("#idme",:text =>"Additional Military Discount applied through ID.me")
+  
+  find(".rate-table dl:nth-child(1) .cfive").click 
+  expect(page).to have_css(".troopSwapSidepanel",:text =>"ID.me Up to 15% Room Discount for Military, First Responders, Students & Teachers. What is ID.me? Verification by ID.me")
+end
+
+Given(/^CAESARS\- I book a room and verify my military discount has been applied$/) do
+  first(".hotel-list #A1").click 
+  expect(page).to have_css(".title dd:nth-child(3)",:text => "Military Discount: 10% - Applied")
+  #NOTE: Caesears doesn't disclose their promotional discount amounts so cannot at this time confirm discount being applied
 end
