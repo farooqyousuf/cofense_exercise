bc9e1fadd3f6a1db8cb5f9a4b2d7c092812cecff
Eddie Park <eddie.park@id.me>
2016-07-27 09:09:33 -0400

    QA-323: Add irobot cucumber feature and step def outlines

---
+++ features/step_definitions/partner_steps/irobot_steps.rb
@@ -0,0 +1,15 @@
+Given(/^IROBOT\- I visit the iRobot HLP page$/) do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Given(/^IROBOT\- I log in with my Id\.me military account$/) do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Given(/^IROBOT\- I select a iRobot Roomba and add to checkout$/) do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Given(/^IROBOT\- I verify that my IDme military discount has been applied$/) do
+  pending # Write code here that turns the phrase above into concrete actions
+end
