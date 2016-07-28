398c53023d402ec15da7a274fb4beda291264869
Eddie Park <eddie.park@id.me>
2016-07-28 17:55:10 -0400

    QA-328: Add JR Cigar First Responder Integration

--- features/step_definitions/partner_steps/jr_cigar_steps.rb
+++ features/step_definitions/partner_steps/jr_cigar_steps.rb
@@ -1,18 +1,15 @@
 Given(/^JRCIGAR \- I verify the military discount landing page$/) do
 	visit FigNewton.partners.jr_cigar_landing_page
 	expect(page).to have_css(".page-static-military-discount h1",:text => "Military & First Responder Discount")
-	#expect(page).to have_css(".page-static-military-discount .idme-verification-container",:text => "JR Cigar Customers Save with ID.ME")
 	sleep 1
 	page.driver.browser.navigate.refresh
 	click_link "Log In or Sign Up"
 end
 
 Given(/^JRCIGAR \- I login to JR Cigar$/) do
-	find("form[name='registerForm'] input[type='email']").set(FigNewton.partners.user_email_random)
-	find("form[name='registerForm'] input[name='password']").set(FigNewton.partners.password)
-	find("form[name='registerForm'] input[name='confirm_password']").set(FigNewton.partners.password)
-	click_button "Register"
-	find(:link,:text =>"Skip and start shopping").click 
+	find("form[name='loginForm'] input[type='email']").set(FigNewton.partners.user)
+	find("form[name='loginForm'] input[name='password']").set(FigNewton.partners.password)
+	click_button "Log In"
 end
 
 Given(/^JRCIGAR \- I add item to the cart$/) do
@@ -42,6 +39,23 @@ Given(/^JRCIGAR \- I login with my military Idme account$/) do
 end
 
 Given(/^JRCIGAR \- I verify my IDme military discount has been applied$/) do
+	binding.pry
+	expect(page).to have_css(".coupon-title",:match => :first, :text =>"Coupon 10MILT-AFFILIATES Save $10.84")
+	expect(page).to have_css("a[href='#military-discount']",:text => "MILITARY DISCOUNT APPLIES TO YOUR ORDER!")
+end
+
+Given(/^JRCIGAR \- I login with my First Responder Idme account$/) do
+	idp_signin = window_opened_by do
+		find(".idme-btn-alt-lg-Responder").click
+	end 
+
+	within_window idp_signin do
+		sign_in_with_idme
+	end 
+end
+
+Given(/^JRCIGAR \- I verify my IDme First Responder discount has been applied$/) do
+	binding.pry
 	expect(page).to have_css(".coupon-title",:match => :first, :text =>"Coupon 10MILT-AFFILIATES Save $10.84")
 	expect(page).to have_css("a[href='#military-discount']",:text => "MILITARY DISCOUNT APPLIES TO YOUR ORDER!")
 end
