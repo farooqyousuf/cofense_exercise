a00f11dbdc53d6077051121020933ef70b54e6b1
Eddie Park <eddie.park@id.me>
2016-08-02 09:03:29 -0400

    QA-356: Add Merrell feature steps and step defs

---
+++ features/partner_features/merrell.feature
@@ -0,0 +1,7 @@
+@partners @version2 @desktop @merrell
+Feature: Test Merrell Partner Integration
+
+Scenario: Test Merrell Military Integration
+	* MERRELL- I add a item to the cart
+	* MERRELL- I signin with my Id.me military account
+	* MERRELL- I verify my military discount was applied
---
+++ features/step_definitions/partner_steps/merrell_steps.rb
@@ -0,0 +1,11 @@
+Given(/^MERRELL\- I add a item to the cart$/) do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Given(/^MERRELL\- I signin with my Id\.me military account$/) do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Given(/^MERRELL\- I verify my military discount was applied$/) do
+  pending # Write code here that turns the phrase above into concrete actions
+end
