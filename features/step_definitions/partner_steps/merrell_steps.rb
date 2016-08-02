509dc2956ad4d910c7c3bc511d9833653c78944a
Eddie Park <eddie.park@id.me>
2016-08-02 09:54:36 -0400

    QA-356: Add Merrell Military Partner test

--- features/step_definitions/partner_steps/merrell_steps.rb
+++ features/step_definitions/partner_steps/merrell_steps.rb
@@ -1,11 +1,30 @@
 Given(/^MERRELL\- I add a item to the cart$/) do
-  pending # Write code here that turns the phrase above into concrete actions
+	visit FigNewton.partners.merrell
+	page.driver.browser.navigate.refresh #remove email modal
+
+	find("#variationsdropdown-24572M").click
+	find("label[id='swatch-L']").click
+	find(".variations-other").click
+	find("#add-to-cart").click
+
+	visit "http://www.merrell.com/US/en/cart"
 end
 
 Given(/^MERRELL\- I signin with my Id\.me military account$/) do
-  pending # Write code here that turns the phrase above into concrete actions
-end
+  idp_signin = window_opened_by do
+    find(".login-trigger").click
+  end
+
+  within_window idp_signin do
+    sign_in_with_idme
+	end
+end 
 
 Given(/^MERRELL\- I verify my military discount was applied$/) do
-  pending # Write code here that turns the phrase above into concrete actions
+	expect(page).to have_css(".IDmeButtonMil",:visible => true)
+	expect(page).to have_css(".order-discount",:visible => true)
+	original_product_amt_string = find(".order-subtotal td:nth-child(2)").text
+	actual_product_discounted_amt_string = find(".order-discount td:nth-child(2)").text
+	discount_applied = verify_discount(original_product_amt_string,actual_product_discounted_amt_string,".10")
+	expect(discount_applied).to be(true)
 end
