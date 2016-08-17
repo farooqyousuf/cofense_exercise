966f32ef177e5aef84955968ea64395555eaf9e0
Eddie Park <eddie.park@id.me>
2016-08-17 11:31:40 -0400

    QA-358: Add Rhone partner military integration test

---
+++ features/step_definitions/partner_steps/rhone_steps.rb
@@ -0,0 +1,20 @@
+Given(/^RHONE \- I add a item to the cart$/) do
+	visit FigNewton.partners.rhone 
+	first(".sizes li").click 
+	find(".product-details__addtocart").click 
+	find(".main-nav__aux-bag").click
+	find(".sidecart-actions__checkout").click 
+end
+
+Given(/^RHONE \- I sign in with my Idme military account$/) do
+	find(".idMeHeader").click 
+	first(".idme-btn-primary-lg-Student").click 
+	sign_in_with_idme
+end 
+
+Given(/^RHONE \- I verify my military discount was applied$/) do
+	expect(page).to have_text("ID.ME-SERVICE MEMBER")
+	original_product_amt_string = find(".order-summary__section--total-lines .total-line--subtotal .total-line__price").text
+	actual_product_discounted_amt_string = find(".order-summary__section--total-lines .total-line--reduction .total-line__price").text
+	discount_applied = verify_discount(original_product_amt_string,actual_product_discounted_amt_string,".15",:exact_match => true)
+end 
