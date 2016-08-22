111b4542c77bbbfb3b1d006ec36826c96de62e00
Eddie Park <eddie.park@id.me>
2016-08-22 18:03:36 -0400

    QA-343: Add comments and set url for wallet homepage

--- features/step_definitions/wallet_steps/wallet_user_steps.rb
+++ features/step_definitions/wallet_steps/wallet_user_steps.rb
@@ -1,19 +1,29 @@
 Given(/^I visit the Wallet homepage$/) do
-  pending # Write code here that turns the phrase above into concrete actions
+	visit FigNewton.wallet.homepage 
+	#add some basic verifications of homepage here?
 end
 
 Given(/^I click on the Wallet Sign Up link$/) do
-  pending # Write code here that turns the phrase above into concrete actions
+	click_link "Sign Up" #might have to turn into first
 end
 
 Given(/^I should be on the marketplace landing page$/) do
-  pending # Write code here that turns the phrase above into concrete actions
+  #check the page url is correct
+  # check the user name matches?
+  # check maybe some page text is available 
+  binding.pry
+  expect(page.current_url).to eql(FigNewton.)
 end
 
 Given(/^I click on the Wallet shared nav link$/) do
   pending # Write code here that turns the phrase above into concrete actions
+  #this should be a page object by now however 
+  #click link for it 
 end
 
 Given(/^I should be on the Wallet dashboard page$/) do
   pending # Write code here that turns the phrase above into concrete actions
+  #confirm page url
+  #check user url
+  #check that page section portions are in fact available 
 end
