ef450019b69c42c5dd8afd2fcb74fdb271a622cb
Farooq <farooqyousuf1@gmail.com>
2016-09-13 15:47:31 -0400

    HOTFIX: commented out screenshots on failures, need to fix later

--- features/support/hooks.rb
+++ features/support/hooks.rb
@@ -9,22 +9,24 @@ After do
   Capybara.current_session.driver.quit
 end
 
-After do |scenario|
-  if scenario.failed?
-    file = "FAILED_#{scenario.name.gsub(" ","_").gsub(/[^0-9A-Za-z_]/, "")}.png"
+#Have to revisit this and fix it
+# After do |scenario|
+#   if scenario.failed?
+#     file = "FAILED_#{scenario.name.gsub(" ","_").gsub(/[^0-9A-Za-z_]/, "")}.png"
 
-    # create directory for images
-    Dir.mkdir("./screenshots") unless Dir.exists?("./screenshots")
+#     # create directory for images
+#     # Dir.mkdir("./screenshots") unless Dir.exists?("./screenshots")
+#     Dir.mkdir("./screenshots") unless Dir.exists?("./screenshots")
 
-    # save the file locally
-    page.save_screenshot("#{Dir.pwd}/screenshots/#{file}")
+#     # save the file locally
+#     page.save_screenshot("#{Dir.pwd}/screenshots/#{file}")
 
-    if AllureCucumber::FeatureTracker.tracker
-      # attaches failed test screenshot to Allure reports
-      attach_file(file, File.open("#{Dir.pwd}/screenshots/#{file}"))
-    end
-  end
-end
+#     if AllureCucumber::FeatureTracker.tracker
+#       # attaches failed test screenshot to Allure reports
+#       attach_file(file, File.open("#{Dir.pwd}/screenshots/#{file}"))
+#     end
+#   end
+# end
 
 After("@delete_mil_doc_multi_family_users") do
   visit_admin_users_in_new_window
