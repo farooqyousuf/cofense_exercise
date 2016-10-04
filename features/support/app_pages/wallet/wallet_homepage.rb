fd7147f37c643ffb8ad29d6986f6a9f6b1c84545
Eddie Park <eddie.park@id.me>
2016-10-04 17:05:15 -0400

    IDME-2795: Add wallet homepage objects for sign in and up

--- features/support/app_pages/wallet/wallet_homepage.rb
+++ features/support/app_pages/wallet/wallet_homepage.rb
@@ -2,8 +2,8 @@ class WalletHomepage < IDmeBase
   include IDPBase
 
   def initialize
-    super(FigNewton.wallet.homepage)
-  end 
+    super(FigNewton.wallet.homepage.url)
+  end
 
   def sign_in(user_email)
     fill_in_email_by_id(user_email)
@@ -15,5 +15,25 @@ class WalletHomepage < IDmeBase
     fill_in_email_by_id(FigNewton.mp_users.military)
     fill_in_password(FigNewton.mp_users.incorrect_password)
     click_sign_in
-  end 
-end 
+  end
+
+  def click_shared_nav_sign_in_button
+    find(".shared-nav-login a",:text =>"Sign In").click
+  end
+
+  def click_shared_nav_sign_up_button
+    find(".shared-nav-login a",:text =>"Sign Up").click
+  end
+
+  def click_hero_sign_up_button
+    find(".wallet-home-hero-cta a",:text =>"Sign Up For Free").click
+  end
+
+  def click_portable_sign_up_button
+    find(".wallet-home-portable-content a",:text =>"Get Started Now").click
+  end
+
+  def click_home_cta_sign_up_button
+    find(".wallet-home-cta a",:text =>"Get Started Now").click
+  end
+end
