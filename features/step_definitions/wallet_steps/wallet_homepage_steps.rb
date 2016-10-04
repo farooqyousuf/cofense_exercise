d34767f3b62b4044de06e182ffe449c3fbb90110
Eddie Park <eddie.park@id.me>
2016-10-04 17:02:19 -0400

    IDME-2795: Add Wallet homepage smoke step defs

---
+++ features/step_definitions/wallet_steps/wallet_homepage_steps.rb
@@ -0,0 +1,43 @@
+Given(/^I verify I am on the wallet homepage$/) do
+  expect(page.current_url).to eql(FigNewton.wallet.homepage.url)
+  expect(page.title).to eql(FigNewton.wallet.homepage.title)
+end
+
+Given(/^I verify that the wallet home hero is displayed$/) do
+  expect(page).to have_css(".content-container",:text => "One Identity. One Wallet. ID.me Wallet empowers you to manage all of the moving parts of your digital identity. With one login, you'll be able to easily access benefits and discounts from hundreds of partners. Start taking control of your digital identity today.")
+  expect(page).to have_css(".wallet-home-hero-cta a",:visible => true)
+end
+
+Given(/^I verify the wallet sign in links direct to idp$/) do
+  @WalletHomepage.click_shared_nav_sign_in_button
+  expect(page).to have_current_path(FigNewton.idp.new_session_url,:url => true)
+  return_to_previous_page
+end
+
+Given(/^I verify the wallet sign up links direct to idp$/) do
+  %w[click_shared_nav_sign_up_button click_hero_sign_up_button click_portable_sign_up_button click_home_cta_sign_up_button].each do |method|
+    @WalletHomepage.send(method)
+    expect(page).to have_current_path(FigNewton.idp.new_registration_url,:url => true)
+    return_to_previous_page
+  end
+end
+
+Given(/^I verify the Wallet shared navigation tab "([^"]*)" successfully links$/) do |nav_tab|
+  nav_link_url = case nav_tab
+              when "About"    then FigNewton.idme.about_page
+              when "Business" then FigNewton.idme.business_page
+              when "Shop"     then FigNewton.marketplace.shop_homepage
+              when "Wallet"   then FigNewton.wallet.homepage.url
+              end
+  find(".wallet-header-nav a",:text => nav_tab ).click
+
+  if nav_tab == "Business"
+    #switch to new window
+    #expectation statement
+    #close window and switch to previous
+  else
+    expect(page).to have_current_path(nav_link_url, :url => true)
+    return_to_previous_page
+  end
+end
+
