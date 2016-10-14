969c4dabe7972b975c0523da1d4bf44faa35350e
Eddie Park <eddie.park@id.me>
2016-10-14 10:37:00 -0400

    IDME-2836: Delete extraneous spaces

--- features/step_definitions/wallet_steps/wallet_navigation_steps.rb
+++ features/step_definitions/wallet_steps/wallet_navigation_steps.rb
@@ -1,9 +1,9 @@
 Given(/^I click on the Dashboard navigation link$/) do
-	@WalletNavigation.click_dashboard_link
+  @WalletNavigation.click_dashboard_link
 end
 
 Given(/^I click on the My IDs navigation link$/) do
-	@WalletNavigation.click_myids_link
+  @WalletNavigation.click_myids_link
 end
 
 Given(/^I check the Wallet My IDs page is visible$/) do
@@ -19,17 +19,17 @@ Given(/^I click on the Settings navigation link$/) do
 end
 
 Given(/^I check the Wallet Settings page is visible$/) do
-	expect(page).to have_current_path(FigNewton.wallet.settings,:url => true)
-	expect(page).to have_css(".mod-wallet-toggle-btns-external",:text =>"PROFILE ACCOUNT SECURITY PRIVACY")
-	expect(page).to have_css(".wallet-settings-user")
-	expect(page).to have_css(".wallet-table",:visible => true)
+  expect(page).to have_current_path(FigNewton.wallet.settings,:url => true)
+  expect(page).to have_css(".mod-wallet-toggle-btns-external",:text =>"PROFILE ACCOUNT SECURITY PRIVACY")
+  expect(page).to have_css(".wallet-settings-user")
+  expect(page).to have_css(".wallet-table",:visible => true)
 end
 
 Given(/^I click on the Activity navigation link$/) do
-	@WalletNavigation.click_activity_link
+  @WalletNavigation.click_activity_link
 end
 
 Given(/^I check that Wallet Activity page is visible$/) do
-	expect(page).to have_current_path(FigNewton.wallet.activity,:url => true)
-	expect(page).to have_css(".wallet-content-main .heading",:text =>"Activity Feed")
+  expect(page).to have_current_path(FigNewton.wallet.activity,:url => true)
+  expect(page).to have_css(".wallet-content-main .heading",:text =>"Activity Feed")
 end
