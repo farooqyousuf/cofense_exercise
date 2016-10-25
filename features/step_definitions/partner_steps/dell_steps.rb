ed75b11795f5fdf5ca88e16ba03c8da69dec7d82
Eddie Park <eddie.park@id.me>
2016-10-25 18:03:31 -0400

    IDME-2897: Update Dell Page title matcher

--- features/step_definitions/partner_steps/dell_steps.rb
+++ features/step_definitions/partner_steps/dell_steps.rb
@@ -6,9 +6,8 @@ Given(/^DELL \- visit HLP page and verify coupon code$/) do
 	within_window idp_sign_in do 
 		sign_in_with_idme
 	end 
-	#@promo_code = find(".show-code").text
 	click_link "Continue »"
-	expect(page.title).to have_text "Dell Laptops, Desktop, All-In-Ones | Member Purchase Program | Dell"
+	expect(page.title).to have_text "Welcome to the Member Purchase Program | Dell"
 end
 
 # TODO: currently blocked for verifying promocode because of rate limit 
