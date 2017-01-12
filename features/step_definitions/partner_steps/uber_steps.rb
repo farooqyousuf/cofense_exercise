db011157ba19acba1476fee9494e524671ebb97a
Eddie Park <eddie.park@id.me>
2017-01-12 11:20:01 -0500

    IDME-3166: Add Uber cucumber feature and step definitions

---
+++ features/step_definitions/partner_steps/uber_steps.rb
@@ -0,0 +1,11 @@
+Given(/^UBER \- I visit the UBER hosted landing page$/) do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Given(/^UBER \- I sigin in with my IDme military account$/) do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Given(/^UBER \- I confirm my IDme military account has been verified$/) do
+  pending # Write code here that turns the phrase above into concrete actions
+end
