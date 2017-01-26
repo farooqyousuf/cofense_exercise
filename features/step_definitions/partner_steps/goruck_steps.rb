173bbfcbb1ec01c941098bfe95453a1937a4560a
pamelaolney <pjolney12@gmail.com>
2017-01-26 11:59:29 -0500

    IDME-3176: Added student steps for integration

---
+++ features/step_definitions/partner_steps/goruck_steps.rb
@@ -0,0 +1,11 @@
+Given(/^GORUCK STUDENT \- I add a item to the cart$/) do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Given(/^GORUCK STUDENT \- I sign in with my IDme Student account$/) do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Given(/^GORUCK STUDENT \- I verify that my student discount was applied$/) do
+  pending # Write code here that turns the phrase above into concrete actions
+end
