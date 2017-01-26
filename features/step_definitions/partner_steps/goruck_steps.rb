272e6b05bd60f31f19cb7531d77ce3429360383b
pamelaolney <pjolney12@gmail.com>
2017-01-26 14:32:14 -0500

    IDME-3176: Added test to verify discount for student - currently shows false

--- features/step_definitions/partner_steps/goruck_steps.rb
+++ features/step_definitions/partner_steps/goruck_steps.rb
@@ -13,5 +13,10 @@ Given(/^GORUCK STUDENT \- I sign in with my IDme Student account$/) do
 end
 
 Given(/^GORUCK STUDENT \- I verify that my student discount was applied$/) do
-  pending # Write code here that turns the phrase above into concrete actions
+  page.has_text? ("Earned Service Discount")
+  original_product_amt_string = find(".is-crossedout").text
+  actual_product_discounted_amt_string = find("#mz-carttable-total").text
+
+  discount_applied = verify_discount(original_product_amt_string, actual_product_discounted_amt_string, ".25",:verify_discount_total => true)
+  expect(discount_applied).to be(true)
 end
