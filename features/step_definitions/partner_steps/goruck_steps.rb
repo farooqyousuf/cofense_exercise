23e97fd65845d6bc67a5e1e84598ee29d212d4df
pamelaolney <pjolney12@gmail.com>
2017-01-26 13:57:57 -0500

    IDME-3176: Student is able to sign in with IDme account

--- features/step_definitions/partner_steps/goruck_steps.rb
+++ features/step_definitions/partner_steps/goruck_steps.rb
@@ -5,7 +5,11 @@ Given(/^GORUCK STUDENT \- I add a item to the cart$/) do
 end
 
 Given(/^GORUCK STUDENT \- I sign in with my IDme Student account$/) do
-  pending # Write code here that turns the phrase above into concrete actions
+  find("#gr-idme-student").click
+  find("#user_email").set FigNewton.test_user.student.user_email
+  find("#user_password").set FigNewton.test_user.password
+  click_on("Sign in")
+  click_on("Continue")
 end
 
 Given(/^GORUCK STUDENT \- I verify that my student discount was applied$/) do
