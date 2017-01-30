e816160e076a978587d8085fc882aff74dabc820
pamelaolney <pjolney12@gmail.com>
2017-01-30 16:15:29 -0500

    IDME-3244: Troop can sign into IDP

--- features/step_definitions/partner_steps/yeti_steps.rb
+++ features/step_definitions/partner_steps/yeti_steps.rb
@@ -1,10 +1,27 @@
 Given(/^YETI\- I add an item to the cart$/) do
-  FigNewton.partners.yeti.product_page
-  puts "Visited the partner page"
+  visit FigNewton.partners.yeti.product_page
+  click_button("Add to Cart")
+  puts "Added product to cart"
+  click_link("VIEW CART")
+  puts "Viewed cart"
 end
 
-Given(/^YETI\- I verify my "([^"]*)" through ID\.me$/) do |arg1|
-  pending # Write code here that turns the phrase above into concrete actions
+Given(/^YETI\- I verify my "([^"]*)" through ID\.me$/) do |group|
+
+  user_group = case group
+  when "troop_id"
+    "military"
+  end
+
+  idp_signin = window_opened_by do
+    find(".button-container a[data-scope='#{user_group}']").click
+  end
+
+  within_window idp_signin do
+    sign_in_with_idme_account(group)
+  end
+
+  puts "#{user_group} successfully opened idp window"
 end
 
 Given(/^YETI\- I verify my "([^"]*)" discount has been applied$/) do |arg1|
