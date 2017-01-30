457986c43ca9a3c1f7bc88af1ad2157b865b47ec
pamelaolney <pjolney12@gmail.com>
2017-01-30 15:52:40 -0500

    IDME-3244: Created yeti steps and began building first step

---
+++ features/step_definitions/partner_steps/yeti_steps.rb
@@ -0,0 +1,12 @@
+Given(/^YETI\- I add an item to the cart$/) do
+  FigNewton.partners.yeti.product_page
+  puts "Visited the partner page"
+end
+
+Given(/^YETI\- I verify my "([^"]*)" through ID\.me$/) do |arg1|
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Given(/^YETI\- I verify my "([^"]*)" discount has been applied$/) do |arg1|
+  pending # Write code here that turns the phrase above into concrete actions
+end
