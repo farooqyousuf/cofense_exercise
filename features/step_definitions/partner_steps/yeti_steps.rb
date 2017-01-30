76b745ee1a220e3dff63ef3932a542aa446caf3f
pamelaolney <pjolney12@gmail.com>
2017-01-30 16:49:53 -0500

    IDME-3244: Discount has successfully been applied and verified to military group

--- features/step_definitions/partner_steps/yeti_steps.rb
+++ features/step_definitions/partner_steps/yeti_steps.rb
@@ -29,6 +29,12 @@ Given(/^YETI\- I verify my "([^"]*)" discount has been applied$/) do |group|
   when "troop_id"
       "military"
     end
-    
+
   page.has_text? ("YOU HAVE SUCCESSFULLY VERIFIED YOURSELF AS")
+  original_product_amt_string = find("#shopping-cart-totals-table > tbody > tr:nth-child(1) > td.a-right > span").text
+  actual_product_discounted_amt_string = find("#shopping-cart-totals-table > tbody > tr:nth-child(2) > td.a-right > i > span").text
+
+  discount_applied = verify_discount(original_product_amt_string, actual_product_discounted_amt_string, ".15", :exact_match => true)
+  expect(discount_applied).to be(true)
+  puts "#{user_group} discount has successfully been applied"
 end
