acb351ecc2475f3c5f12ddb1801f500f8e64ab3f
Eddie Park <eddie.park@id.me>
2017-01-30 09:58:41 -0500

    IDME-3208: Update MLB shop test to use verify_discount method

--- features/step_definitions/partner_steps/mlb_shop_steps.rb
+++ features/step_definitions/partner_steps/mlb_shop_steps.rb
@@ -1,5 +1,5 @@
 Given(/^MLBSHOP \- I add an item to the cart$/) do
-  visit FigNewton.partners.mlb_shop
+  visit FigNewton.partners.mlb_shop.product_page
   page.driver.browser.navigate.refresh
   visit "http://www.mlbshop.com/Baltimore_Orioles_Jerseys/Mens_Baltimore_Orioles_Manny_Machado_Majestic_Orange_Alternate_Cool_Base_Player_Jersey"
   find(".sizeChoiceContainer a[title='Choose Size S']").click
@@ -16,9 +16,10 @@ Given(/^MLBSHOP \- I apply the Troop ID discount$/) do
 end
 
 Given(/^MLBSHOP \- I verify the Troop ID discount has been applied$/) do
-  #TODO: refactor this to use the verify_discount method
-  expect(page).to have_css(".ui-alert-body",:text => FigNewton.partners.mlb_shop_discount)
-  expect(page).to have_selector("#checkoutCouponTotalValue", :visible => true)
-  expect(page).to have_css("#checkoutCouponTotalValue",:text => FigNewton.partners.mlb_shop_discount_amount)
-  expect(page).to have_selector("#desktopIdMeVerified",:text =>"Status Verified",:visible => false)
+  expect(page).to have_css(".ui-alert-body",:text => FigNewton.partners.mlb_shop.discount_copy)
+  
+  original_product_amt_string = find(".displayTotalPrice").text
+  actual_product_discounted_amt_string = find("#checkoutCouponTotalValue").text
+  discount_applied = verify_discount(original_product_amt_string,actual_product_discounted_amt_string,".15")
+  expect(discount_applied).to be(true)
 end
