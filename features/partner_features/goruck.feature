0fae2bf2ca31683fa93059f8bfbad7cf95d4d832
pamelaolney <pjolney12@gmail.com>
2017-01-30 14:09:28 -0500

    IDME-3176: Discounts are verified for each group

--- features/partner_features/goruck.feature
+++ features/partner_features/goruck.feature
@@ -9,7 +9,7 @@ Feature: Test the ID.me Goruck Partner Integration
   Scenario: Test Goruck Teacher Integration
     * GORUCK- I add a item to the cart
     * GORUCK- I sign in with my IDme "teacher_id" account
-    * GORUCK - I verify that my "teacher_id" discount was applied
+    * GORUCK- I verify that my "teacher_id" discount was applied
 
 
   Scenario: Test Goruck Student Integration
--- features/step_definitions/partner_steps/goruck_steps.rb
+++ features/step_definitions/partner_steps/goruck_steps.rb
@@ -44,14 +44,26 @@ Given(/^GORUCK\- I sign in with my IDme "([^"]*)" account$/) do |group|
   puts "#{user_group} was selected"
   sign_in_with_idme_account(group)
   puts "#{user_group} has signed in"
-
 end
 
 
-Given(/^GORUCK\- I verify that my "([^"]*)" discount was applied$/) do |arg1|
-  pending # Write code here that turns the phrase above into concrete actions
-end
+Given(/^GORUCK\- I verify that my "([^"]*)" discount was applied$/) do |group|
+  user_group = case group
+    when "troop_id"
+      "military"
+    when "teacher_id"
+      "teacher"
+    when "responder_id"
+      "firstResponder"
+    when "student_id"
+      "student"
+  end
 
-Given(/^GORUCK \- I verify that my "([^"]*)" discount was applied$/) do |arg1|
-  pending # Write code here that turns the phrase above into concrete actions
+  page.has_text? ("Earned Service Discount")
+  original_product_amt_string = find(".is-crossedout").text
+  actual_product_discounted_amt_string = find(".mz-item-discount").text
+
+  discount_applied = verify_discount(original_product_amt_string, actual_product_discounted_amt_string, ".25",:exact_match => true)
+  expect(discount_applied).to be(true)
+  puts "#{user_group} discount has successfully been applied"
 end
