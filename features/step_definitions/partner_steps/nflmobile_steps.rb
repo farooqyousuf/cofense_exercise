0837a18f82fa3b388f827d95b9af3564f0474b50
Eddie Park <eddie.park@id.me>
2017-01-31 10:12:38 -0500

    IDME-3250: Add completed NFL mobile step definition

--- features/step_definitions/partner_steps/nflmobile_steps.rb
+++ features/step_definitions/partner_steps/nflmobile_steps.rb
@@ -1,24 +1,22 @@
-Given(/^NFLMobile \- I add an item to the cart$/) do
-  visit FigNewton.partners.nfl_shop
-  page.driver.browser.navigate.refresh
+Given(/^NFL_M \- I add an item to the cart$/) do
+  visit FigNewton.partners.nfl_mobile.product_page
+  select_product_size_for_partner("nfl_mobile")
+  find("#mobileAddToCart").click
   sleep 1
-  click_button "S"
-  sleep 1
-  click_on "mobileAddToCart"
 end
 
-Given(/^NFLMobile \- I apply the Troop ID discount$/) do
-  sleep 1
-  find_link("militaryAndFirstResponderHeader").click
-  sleep 1
-  find(".idMeMilitaryBtn").click
+Given(/^NFL_M \- I apply the Troop ID discount$/) do
+  find("#militaryAndFirstResponderHeader").click
 
-  sign_in_with_idme
-  sleep 3
+  idp_signin = window_opened_by { find("#imgMilitary").click }
+    within_window idp_signin do
+      sign_in_with_idme
+    end
 end
 
-Given(/^NFLMobile \- I verify the Troop ID discount has been applied$/) do
-  sleep 3
-  page.assert_text("Status Verified", :wait => 300)
-  page.assert_text("A discount of 15% and free shipping was successfully applied to your order.",:wait => 300)
+Given(/^NLF_M \- I verify the Troop ID discount has been applied$/) do
+  original_product_amt_string = find(".cartTotalAmount").text
+  actual_product_discounted_amt_string = find(".couponAmount").text
+  discount_applied = verify_discount(original_product_amt_string,actual_product_discounted_amt_string, ".10")
+  expect(discount_applied).to be(true)
 end
