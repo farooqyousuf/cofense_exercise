367f99f08d433dbf5f092d1efd1b81dee15ae372
pamelaolney <pjolney12@gmail.com>
2017-02-06 14:20:34 -0500

    INT-972: Able to successfully log into the admin tool

---
+++ features/step_definitions/support_steps/admin_steps/add_user_steps.rb
@@ -0,0 +1,13 @@
+Given(/^I visit the admin tool$/) do
+  @admin_tool = AdminTool.new
+  @admin_tool.login_in_new_window
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Given(/^I create user$/) do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Given(/^I verify new user was created$/) do
+  pending # Write code here that turns the phrase above into concrete actions
+end
