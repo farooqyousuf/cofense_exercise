790d1212e294349f7f2faad95af39c170f84177d
pamelaolney <pjolney12@gmail.com>
2017-02-06 14:43:37 -0500

    INT-972: Able to select Users from Member dropdown

--- features/step_definitions/support_steps/admin_steps/add_user_steps.rb
+++ features/step_definitions/support_steps/admin_steps/add_user_steps.rb
@@ -1,11 +1,11 @@
 Given(/^I visit the admin tool$/) do
   @admin_tool = AdminTool.new
   @admin_tool.login_in_new_window
-  pending # Write code here that turns the phrase above into concrete actions
 end
 
 Given(/^I create user$/) do
-  pending # Write code here that turns the phrase above into concrete actions
+  click_link("Members")
+  click_link("Users")
 end
 
 Given(/^I verify new user was created$/) do
