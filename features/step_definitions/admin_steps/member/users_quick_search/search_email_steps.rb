aeca0d536a41ea77a5b48255a0337abfba610c95
pamelaolney <pjolney12@gmail.com>
2017-02-08 13:27:34 -0500

    INT-968: Added skeleton steps to file and added new member folder for clarity

---
+++ features/step_definitions/admin_steps/member/users_quick_search/search_email_steps.rb
@@ -0,0 +1,11 @@
+Given(/^I visit the user quick search page$/) do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Given(/^I search user by email$/) do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Given(/^I verify that the user was searched$/) do
+  pending # Write code here that turns the phrase above into concrete actions
+end
