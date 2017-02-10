664ec4d100dcc08ce774d1beb33433e9a47b1a0e
pamelaolney <pjolney12@gmail.com>
2017-02-10 13:09:47 -0500

    INT-968: Added other search methods

--- features/support/app_pages/admin/user_search.rb
+++ features/support/app_pages/admin/user_search.rb
@@ -2,12 +2,18 @@ class UserSearch
 
 include Capybara::DSL
 
-    def search_email_user
+    def search_user_email
+      fill_in("query", with: "capybara+471715@id.me")
+      click_button("Search")
+    end
+    
+    def search_user_full_name
       fill_in("query", with: "capybara+471715@id.me")
       click_button("Search")
     end
 
     def search_user_first_name
-      
+      fill_in("query", with: "capybara+471715@id.me")
+      click_button("Search")
     end
 end
