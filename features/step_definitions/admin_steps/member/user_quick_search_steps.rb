29767ef2bf76e5ad158938dd1dcd178fe9c204e4
pamelaolney <pjolney12@gmail.com>
2017-02-10 13:59:11 -0500

    INT-968: Added case statements to steps

--- features/step_definitions/admin_steps/member/user_quick_search_steps.rb
+++ features/step_definitions/admin_steps/member/user_quick_search_steps.rb
@@ -5,13 +5,25 @@ Given(/^I visit the user quick search page$/) do
 end
 
 Given(/^I search user by "([^"]*)"$/) do |type|
-  @user_email_search = UserSearch.new
-  if type == "email"
-    @user_email_search.search_email_user
-  end
+  @search_user = UserSearch.new
+  case type
+    when "email"
+      @search_user.search_user_email
+    when "full_name"
+      @search_user.search_user_full_name
+    when "first_name"
+      @search_user.search_user_first_name
+    end
 end
 
-Given(/^I verify that the user was searched$/) do
-  click_link("Capybara DoNotDelete")
-  expect(page).to have_text("Capybara DoNotDelete")
+Given(/^I verify that the user was searched by "([^"]*)"$/) do |type|
+  case type
+  when "email"
+    click_link("Capybara DoNotDelete")
+    expect(page).to have_text("Capybara DoNotDelete")
+  when "full_name"
+    puts "verify full_name"
+  when "first_name"
+    puts "verify first_name"
+  end
 end
