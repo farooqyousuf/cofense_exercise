b838b23a6ee36e227ac2af08f4571186d74b404c
pamelaolney <pjolney12@gmail.com>
2017-02-13 17:38:51 -0500

    INT-968: Added DataMagic for searching user with email

--- features/step_definitions/admin_steps/member/user_quick_search_steps.rb
+++ features/step_definitions/admin_steps/member/user_quick_search_steps.rb
@@ -5,15 +5,16 @@ Given(/^I visit the user quick search page$/) do
 end
 
 Given(/^I search user by "([^"]*)"$/) do |type|
-  @search_user = UserSearch.new
-  case type
-    when "email"
-      @search_user.search_user_email
-    when "full_name"
-      @search_user.search_user_full_name
-    when "first_name"
-      @search_user.search_user_first_name
-    end
+  UserQuickSearch.new.search_user(type: type)
+  # @search_user = UserQuickSearch.new
+  # case type
+  #   when "email"
+  #     @search_user.search_user_email
+  #   when "full_name"
+  #     @search_user.search_user_full_name
+  #   when "first_name"
+  #     @search_user.search_user_first_name
+  #   end
 end
 
 Given(/^I verify that the user was searched by "([^"]*)"$/) do |type|
--- features/support/app_pages/admin/admin_user_search.rb
+++ features/support/app_pages/admin/admin_user_search.rb
@@ -1,19 +1,43 @@
 class UserQuickSearch
 
 include Capybara::DSL
+include DataMagic
 
-    def search_user_email
-      fill_in("query", with: "capybara+471715@id.me")
-      click_button("Search")
-    end
+  def search_user(populate: true, type: "none")
 
-    def search_user_full_name
-      fill_in("query", with: "Capybara DoNotDelete")
-      click_button("Search")
-    end
+    if populate
 
-    def search_user_first_name
-      fill_in("query", with: "Capybara")
-      click_button("Search")
+      data = data_for(:mil_email)
+
+      case type
+      when "email"
+        populate_field(search_box: data.fetch("dupe_email"))
+      when "full_name"
+        populate_field(search_box: data.fetch("dupe_fname" "dupe_lname"))
+      when "first_name"
+        populate_field(search_box: data.fetch("dupe_fname"))
+      end
     end
+      click_button("Search")
+  end
+
+
+  def populate_field(search_box:)
+    fill_in("query", :with => search_box)
+  end
+
+    # def search_user_email
+    #   fill_in("query", with: "capybara+471715@id.me")
+    #   click_button("Search")
+    # end
+    #
+    # def search_user_full_name
+    #   fill_in("query", with: "Capybara DoNotDelete")
+    #   click_button("Search")
+    # end
+    #
+    # def search_user_first_name
+    #   fill_in("query", with: "Capybara")
+    #   click_button("Search")
+    # end
 end
