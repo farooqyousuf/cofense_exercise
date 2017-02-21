0cc2c52acd6d8f8333162717b98defa66ca972e0
pamelaolney <pjolney12@gmail.com>
2017-02-21 16:10:43 -0500

    INT-968: Removed search method and added functionality to verify method to search

--- features/support/app_pages/admin/member/user_quick_search.rb
+++ features/support/app_pages/admin/member/user_quick_search.rb
@@ -7,34 +7,42 @@ include DataMagic
     super(FigNewton.admin.user_quick_search)
   end
 
-  def search_user(populate: true, type: "none")
-    if populate
-      data = data_for(:admin_test_user)
-      case type
-      when "email"
-        populate_field(search_box: data.fetch("email"))
-      when "full_name"
-        populate_field(search_box: data.fetch("first_name") + " " + data.fetch("last_name"))
-      when "first_name"
-        populate_field(search_box: data.fetch("first_name"))
-      end
-    end
-      click_button("Search")
-  end
+  # def search_user(populate: true, type: "none")
+  #   if populate
+  #     data = data_for(:admin_test_user)
+  #     case type
+  #     when "email"
+        # populate_field(search_box: data.fetch("email"))
+  #     when "full_name"
+  #       populate_field(search_box: data.fetch("first_name") + " " + data.fetch("last_name"))
+  #     when "first_name"
+  #       populate_field(search_box: data.fetch("first_name"))
+  #     end
+  #   end
+  #     click_button("Search")
+  # end
 
   def populate_field(search_box:)
     fill_in("query", :with => search_box)
   end
 
-  def verify(type:)
-    data = data_for(:admin_test_user)
-    case type
-    when "email"
-      click_link(data.fetch("email"))
-    when "full_name"
-      click_link(data.fetch("first_name") + " " + data.fetch("last_name"))
-    when "first_name"
-      click_link(data.fetch("first_name"))
+  def verify(populate: true, type: "none")
+    if populate
+      data = data_for(:admin_test_user)
+      case type
+      when "email"
+        populate_field(search_box: data.fetch("email"))
+        click_button("Search")
+        click_link(data.fetch("email"))
+      when "full_name"
+        populate_field(search_box: data.fetch("first_name") + " " + data.fetch("last_name"))
+        click_button("Search")
+        click_link(data.fetch("first_name") + " " + data.fetch("last_name"))
+      when "first_name"
+        populate_field(search_box: data.fetch("first_name"))
+        click_button("Search")
+        click_link(data.fetch("first_name"))
+      end
     end
     page.assert_text(data.fetch("first_name") + " " + data.fetch("last_name"))
   end
