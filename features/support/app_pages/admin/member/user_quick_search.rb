729f118c39cf6a857257a7aadee9d68158ebc0bc
pamelaolney <pjolney12@gmail.com>
2017-02-21 16:23:32 -0500

    INT-968: Added click_button search to pop field method for Dry'er code

--- features/support/app_pages/admin/member/user_quick_search.rb
+++ features/support/app_pages/admin/member/user_quick_search.rb
@@ -9,6 +9,7 @@ include DataMagic
 
   def populate_field(search_box:)
     fill_in("query", :with => search_box)
+    click_button("Search")
   end
 
   def verify(populate: true, type: "none")
@@ -17,15 +18,12 @@ include DataMagic
       case type
       when "email"
         populate_field(search_box: data.fetch("email"))
-        click_button("Search")
         click_link(data.fetch("email"))
       when "full_name"
         populate_field(search_box: data.fetch("first_name") + " " + data.fetch("last_name"))
-        click_button("Search")
         click_link(data.fetch("first_name") + " " + data.fetch("last_name"))
       when "first_name"
         populate_field(search_box: data.fetch("first_name"))
-        click_button("Search")
         click_link(data.fetch("first_name"))
       end
     end
