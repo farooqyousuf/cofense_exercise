32ba3d8239bfcc743558e5e823ec8f8cd817b7af
pamelaolney <pjolney12@gmail.com>
2017-02-21 16:12:15 -0500

    INT-968: Cleaned comments from code

--- features/support/app_pages/admin/member/user_quick_search.rb
+++ features/support/app_pages/admin/member/user_quick_search.rb
@@ -7,21 +7,6 @@ include DataMagic
     super(FigNewton.admin.user_quick_search)
   end
 
-  # def search_user(populate: true, type: "none")
-  #   if populate
-  #     data = data_for(:admin_test_user)
-  #     case type
-  #     when "email"
-        # populate_field(search_box: data.fetch("email"))
-  #     when "full_name"
-  #       populate_field(search_box: data.fetch("first_name") + " " + data.fetch("last_name"))
-  #     when "first_name"
-  #       populate_field(search_box: data.fetch("first_name"))
-  #     end
-  #   end
-  #     click_button("Search")
-  # end
-
   def populate_field(search_box:)
     fill_in("query", :with => search_box)
   end
