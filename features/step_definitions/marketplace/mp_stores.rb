85c445efb7367d6deb6e8676788c01c518ce73ee
luminousbeam <jocelyn.jeriah@gmail.com>
2017-03-08 12:45:23 -0500

    IDME-3270: Update stores list filtered by name to reflect current stores displayed in mp_stores.rb

--- features/step_definitions/marketplace/mp_stores.rb
+++ features/step_definitions/marketplace/mp_stores.rb
@@ -1,9 +1,9 @@
 Given(/^I create Marketplace Store Index Page object$/) do
-	@marketplace_stores_page = MarketplaceStoreIndexPage.new 
+	@marketplace_stores_page = MarketplaceStoreIndexPage.new
 end
 
 Given(/^I expect to be at the Marketplace Stores Index Page$/) do
-	expect(page.current_url).to eql(FigNewton.marketplace.stores_index_page) 
+	expect(page.current_url).to eql(FigNewton.marketplace.stores_index_page)
 end
 
 Given(/^I check that the filter is visible$/) do
@@ -15,7 +15,7 @@ Given(/^I check that the Stores index is visible$/) do
 end
 
 Given(/^I verify the stores merchant card$/) do
-	expect(page).to have_css(".stores-list-static li:nth-child(1) .merchant-card",:visible => true) 
+	expect(page).to have_css(".stores-list-static li:nth-child(1) .merchant-card",:visible => true)
 	expect(page).to have_css(".stores-list-static li:nth-child(1) .merchant-card .resource-card__teaser",:visible => true )
 	expect(page).to have_css(".stores-list-static li:nth-child(1) .merchant-card .button",:text =>"View Offers")
 end
@@ -23,9 +23,9 @@ end
 Given(/^I check to sort stores by name$/) do
 	select("name", :from => "filters")
 
-	['100 Percent Pure','123Greetings Store','123Inkjets'].each.with_index(offset=1) do |title,index|
+	['123Greetings Store','123Print', '1800AnyLens'].each.with_index(offset=1) do |title,index|
 		expect(page).to have_css(".stores-list-static li:nth-child(#{index}) .merchant-card figure a[title='#{title}']")
-	end 
+	end
 end
 
 Given(/^I check to sort stores by newest$/) do
@@ -33,7 +33,7 @@ Given(/^I check to sort stores by newest$/) do
 
 	['DISH Network','GlobeIn','Fairway Styles'].each.with_index(offset=1) do |title,index|
 		expect(page).to have_css(".stores-list-static li:nth-child(#{index}) .merchant-card figure a[title='#{title}']")
-	end 
+	end
 end
 
 Given(/^I check to sort stores by highest cash back %$/) do
@@ -41,13 +41,13 @@ Given(/^I check to sort stores by highest cash back %$/) do
 
 	['Bigcommerce','The Shaw Academy','FreeTaxUSA'].each.with_index(offset=1) do |title,index|
 		expect(page).to have_css(".stores-list-static li:nth-child(#{index}) .merchant-card figure a[title='#{title}']")
-	end 
+	end
 end
 
 Given(/^I check to sort stores by highest cash back \$$/) do
 	select("Highest Cash Back $",:from => "filters")
-	
+
 	['DISH Network','KitchenAid','99designs'].each.with_index(offset=1) do |title,index|
 		expect(page).to have_css(".stores-list-static li:nth-child(#{index}) .merchant-card figure a[title='#{title}']")
-	end 
+	end
 end
