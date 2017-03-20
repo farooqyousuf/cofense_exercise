ae325d72152e48888a65a43565907d6ba98869aa
luminousbeam <jocelyn.jeriah@gmail.com>
2017-03-20 14:31:41 -0400

    IDME-3368: Move favorite offer header seelctor to page mp_favorites_page.rb from mp_favorites.rb

--- features/step_definitions/marketplace/mp_favorites.rb
+++ features/step_definitions/marketplace/mp_favorites.rb
@@ -51,7 +51,7 @@ end
 Given(/^I add a favorite store$/) do
   visit "https://shop-staging.idmeinc.net/stores"
   @mp_favorites.add_favorite_store
-  @favorite_offer_header = find(".store-details__grid").find(".heading").text
+  @favorite_offer_header = @mp_favorites.favorite_offer_header
 
   expect(page).to have_css("div.unfavorite-merchant")
 end
