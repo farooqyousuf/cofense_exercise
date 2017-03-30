18436f8f3c99e1e2776a4905c404e3476f9eb8e3
luminousbeam <jocelyn.jeriah@gmail.com>
2017-03-30 14:34:18 -0400

    IDME-3368: Update method selector for side menu bar

--- features/support/app_pages/shop/shop_favorites_page.rb
+++ features/support/app_pages/shop/shop_favorites_page.rb
@@ -6,7 +6,7 @@ class FavoritesPage < IDmeBase
   end
 
   def navigate_from_side_menu_bar
-    click_on("Favorites")
+    find("ul.user-menu__links li:nth-child(2)").click_link("Favorites")
   end
 
   def navigate_from_user_menu_nav
