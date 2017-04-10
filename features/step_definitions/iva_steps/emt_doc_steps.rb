46b30a13a16c1b02812a128e42ed76cac4465cc1
Farooq <farooqyousuf1@gmail.com>
2017-04-10 14:55:41 -0400

    CORE-7206: Added error code tests for EMT Doc upload

--- features/step_definitions/iva_steps/emt_doc_steps.rb
+++ features/step_definitions/iva_steps/emt_doc_steps.rb
@@ -1,3 +1,7 @@
 Given(/^I verify using EMT doc upload$/) do
   DocEMT.new.verify(type: "unique")
 end
+
+Given(/^I submit the EMT document verification form as a "([^"]*)" record$/) do |type|
+  DocEMT.new.verify(type: type)
+end
