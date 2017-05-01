31b6ee59ff21feaad0cdbbd21dbe22636cdc14c5
Eddie Park <eddie.park@id.me>
2017-05-01 08:37:33 -0400

    IDME-3563: Add shop admin categories step def outline

---
+++ features/step_definitions/shop/admin/shop_admin_categories_step.rb
@@ -0,0 +1,19 @@
+Given(/^I navigate to the create a new category page$/) do
+  pending
+end
+
+Given(/^I sumbit a valid new category$/) do
+    pending # Write code here that turns the phrase above into concrete actions
+end
+
+Given(/^I verify the category has been created$/) do
+    pending # Write code here that turns the phrase above into concrete actions
+end
+
+Given(/^I attempt to submit a invalid new category$/) do
+    pending # Write code here that turns the phrase above into concrete actions
+end
+
+Given(/^I verify a error is returned and no new category is created$/) do
+    pending # Write code here that turns the phrase above into concrete actions
+end
