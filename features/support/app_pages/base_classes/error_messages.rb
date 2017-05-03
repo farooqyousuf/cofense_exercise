e28d41e80ff94055a1759ed44d53af10fb7807eb
Farooq <farooqyousuf1@gmail.com>
2017-05-03 15:54:13 -0400

    CORE-7355: Fixed Govt reqd fields test

--- features/support/app_pages/base_classes/error_messages.rb
+++ features/support/app_pages/base_classes/error_messages.rb
@@ -2,15 +2,13 @@ module ErrorMessages
 
   def error_on(field)
     value = (all('.error')[field]['class'])
-    ["error",
-     "select2-container government-affiliation error",
-     "select2-container state-select error",
-     "select2-container dd214-component error",
-     "select2-container dd214-branch error",
-     "select2-container dd214-officer error",
-     "select2-container error",
-     "select2-container service-affiliation error",
-     "select2-container schools validate-me error",
-     "select2-container hide-on-poll state-select error"].include?(value)
+    ["field text left-half error",
+     "field text right-half error",
+     "field text error",
+     "field select affiliation error",
+     "field text left-half error",
+     "field text right-half error",
+     "field select error",
+     "field text error"].include?(value)
   end
 end
