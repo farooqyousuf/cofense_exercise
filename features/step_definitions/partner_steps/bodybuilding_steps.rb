5f1e029ffb11b0414f1275b54e33a218eef5c3ef
pamelaolney <pjolney12@gmail.com>
2017-05-10 16:17:43 -0400

    IDME-3577: Removed window method and changed class name for troop id button

--- features/step_definitions/partner_steps/bodybuilding_steps.rb
+++ features/step_definitions/partner_steps/bodybuilding_steps.rb
@@ -1,35 +1,28 @@
 Given(/^BODYBUILDING \- I verify the Troop ID HLP military page$/) do
   visit FigNewton.partners.bodybuilding_hlp
-
-  idp_signin = window_opened_by do
-    find(".idme-trigger").click
-  end
-
-  within_window idp_signin do
-    sign_in_with_idme
-  end
-	
+  find(".offer-btn").click
+  sign_in_with_idme
 end
 
 Given(/^BODYBUILDING \- I add a item to my cart$/) do
   @coupon_code = find("#couponText").text
 
   visit FigNewton.partners.bodybuilding
-  
+
   find("#OPT2620006-right-submit").click
-  find(".add2cart__footer a:nth-child(2)").click 
-  find("#promo-code-toggler").click 
+  find(".add2cart__footer a:nth-child(2)").click
+  find("#promo-code-toggler").click
   find(".qa-automation-var-couponcode").set(@coupon_code)
 end
 
 Given(/^BODYBUILDING \- I apply the discount code and verify discount applied$/) do
   page.execute_script("window.scrollBy(0,-100)")
-  find(".qa-automation-var-claimcoupon").click 
+  find(".qa-automation-var-claimcoupon").click
   expect(page).to have_text "The Promotional Code #{ @coupon_code } was entered successfully and applied to your cart subtotal."
 
   original_product_amt_string = first(".actual").text
   actual_product_discounted_amt_string = find(".store-discount").find("strong").text
 
-  discount_applied = verify_discount(original_product_amt_string, actual_product_discounted_amt_string, ".10",:exact_match => true) 
+  discount_applied = verify_discount(original_product_amt_string, actual_product_discounted_amt_string, ".10",:exact_match => true)
   expect(discount_applied).to be(true)
 end
