e346436557c8aa040df259572f5339e8393dc196
luminousbeam <jocelyn.jeriah@gmail.com>
2017-05-17 15:48:18 -0400

    IDME-3656: Remove extra spaces

--- features/step_definitions/hlp_steps/hlp_document_page_steps.rb
+++ features/step_definitions/hlp_steps/hlp_document_page_steps.rb
@@ -16,7 +16,7 @@ Given(/^I fill out the required data for the DocumentPage$/) do
 end
 
 Given(/^I should be on the HLP_Test_Partner_Pages page$/) do
-    expect(page.current_url).to eql(FigNewton.hlp.homepage)
+  expect(page.current_url).to eql(FigNewton.hlp.homepage)
 end
 
 Given(/^I delete the original temp_hlp_document_page$/) do
