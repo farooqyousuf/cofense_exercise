0c0f9b0df87d1c4f055841ad35feb6d3de4830a7
pamelaolney <pjolney12@gmail.com>
2017-05-17 12:18:57 -0400

    CORE-7438: Removed username assignment to variable in doc steps file

--- features/step_definitions/iva_steps/document_upload_steps.rb
+++ features/step_definitions/iva_steps/document_upload_steps.rb
@@ -4,6 +4,5 @@ Given(/^I should have successfully verified that a document was uploaded$/) do
 
   step 'I visit "AdminDocs"'
   @admin_docs = AdminDocs.new
-  username = @username
   @admin_docs.verify_doc_upload(@username)
 end
