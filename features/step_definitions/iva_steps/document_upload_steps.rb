f724436deda16cf5ab5f76a46aca66ee3efbf86b
pamelaolney <pjolney12@gmail.com>
2017-05-17 12:12:27 -0400

    CORE-7438: Added new method to verify a doc was successfully uploaded

--- features/step_definitions/iva_steps/document_upload_steps.rb
+++ features/step_definitions/iva_steps/document_upload_steps.rb
@@ -1,9 +1,9 @@
 Given(/^I should have successfully verified that a document was uploaded$/) do
   @admin_tool = AdminTool.new
-  @admin_tool.logout_in_new_window
+  @admin_tool.login_in_new_window
 
   step 'I visit "AdminDocs"'
   @admin_docs = AdminDocs.new
-
-  @admin_docs.verify_doc
+  username = @username
+  @admin_docs.verify_doc_upload(@username)
 end
