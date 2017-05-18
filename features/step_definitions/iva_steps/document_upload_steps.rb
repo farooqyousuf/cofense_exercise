30a564bdd8feaeb670ddc324e049726f17b59701
pamelaolney <pjolney12@gmail.com>
2017-05-18 14:48:36 -0400

    CORE-7439: Added steps for required document upload prompt scenario

--- features/step_definitions/iva_steps/document_upload_steps.rb
+++ features/step_definitions/iva_steps/document_upload_steps.rb
@@ -6,3 +6,11 @@ Given(/^I should have successfully verified that a document was uploaded$/) do
   @admin_docs = AdminDocs.new
   @admin_docs.verify_doc_upload(@username)
 end
+
+Given(/^I submit the military document form for "([^"]*)"$/) do |arg1|
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Given(/^I should see the error message for required document upload$/) do
+  pending # Write code here that turns the phrase above into concrete actions
+end
