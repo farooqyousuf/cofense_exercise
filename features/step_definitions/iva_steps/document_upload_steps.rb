b557de9e3fdcf401c09c46c6cd5588aa3d94b226
pamelaolney <pjolney12@gmail.com>
2017-05-18 15:08:11 -0400

    CORE-7439: Added step to submit military doc form without attaching a document

--- features/step_definitions/iva_steps/document_upload_steps.rb
+++ features/step_definitions/iva_steps/document_upload_steps.rb
@@ -7,8 +7,8 @@ Given(/^I should have successfully verified that a document was uploaded$/) do
   @admin_docs.verify_doc_upload(@username)
 end
 
-Given(/^I submit the military document form for "([^"]*)"$/) do |arg1|
-  pending # Write code here that turns the phrase above into concrete actions
+Given(/^I submit the military document form for "([^"]*)"$/) do |affiliation|
+  MilitaryDoc.new.verify(affiliation: affiliation, type: "no_doc")
 end
 
 Given(/^I should see the error message for required document upload$/) do
