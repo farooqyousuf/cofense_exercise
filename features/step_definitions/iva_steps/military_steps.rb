44ee9fe946ed3ffa4af9c233f423770e87b9c459
pamelaolney <pjolney12@gmail.com>
2017-05-30 15:36:30 -0400

    CORE-7208: Updated conditional statement

--- features/step_definitions/iva_steps/military_steps.rb
+++ features/step_definitions/iva_steps/military_steps.rb
@@ -4,6 +4,7 @@ Given(/^I should be on the military verification screen$/) do
 end
 
 Given(/^I verify using an ID\.me military passcode$/) do
+  step 'I scroll to the bottom of the page'
   click_link("Verify using an ID.me military passcode")
   step 'I click on the Begin link'
   fill_in "verification[passcode]", :with => "ID.me"
