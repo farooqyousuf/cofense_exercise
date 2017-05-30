1fbcd384287b32dd0ff6da1011559bf4531c5686
pamelaolney <pjolney12@gmail.com>
2017-05-30 10:06:11 -0400

    CORE-7269: Added spaces within curly brackets

--- features/support/app_pages/iva/military_email.rb
+++ features/support/app_pages/iva/military_email.rb
@@ -37,7 +37,7 @@ class MilitaryEmail < IDmeBase
       if ["Military Family", "Military Spouse"].include?(affiliation)
         fill_in "verification_first_name", :with => Faker::Name.first_name
         fill_in "verification_last_name", :with => Faker::Name.last_name
-        2.times {fill_in "verification_birth_date", :with => @dob}
+        2.times { fill_in "verification_birth_date", :with => @dob }
         select_option(container_attribute, "#select2-chosen-3", "Veteran", index=0)
       end
 
