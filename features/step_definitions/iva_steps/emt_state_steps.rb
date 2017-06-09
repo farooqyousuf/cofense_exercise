ae51af1233bfe8c36764253f849967d423775ccd
Farooq <farooqyousuf1@gmail.com>
2017-06-09 08:36:33 -0400

    CORE-7621: Fixed emt state lookup happy path test

--- features/step_definitions/iva_steps/emt_state_steps.rb
+++ features/step_definitions/iva_steps/emt_state_steps.rb
@@ -21,3 +21,7 @@ end
 Given(/^I submit the emt state lookup verification form as a "([^"]*)" record$/) do |type|
   StateLookupEMT.new.verify(type: type)
 end
+
+Given(/^I click on the Verify using EMT State link$/) do
+  StateLookupEMT.new.click_verify_emt_state_link
+end
--- features/support/app_pages/iva/emt_state_lookup.rb
+++ features/support/app_pages/iva/emt_state_lookup.rb
@@ -7,10 +7,6 @@ class StateLookupEMT < IDmeBase
   include ErrorMessages
 
   def verify(populate: true, type: nil)
-    find("[data-option=#{container_attribute}]").find(".verification-header").click
-    choose("emt_level_state")
-    populate_first_state("Alaska", unique_id)
-
     if populate
 
       data = data_for(:experian_user) #info for unique and duplicate user
@@ -24,36 +20,40 @@ class StateLookupEMT < IDmeBase
       end
     end
 
-    click_verify_button
+    click_continue
   end
 
   def populate_fields(data:)
-    %w(first_name last_name social social_confirm street city emt_city emt_county emt_number).each do |field|
+    %w(verification_first_name verification_last_name verification_social verification_social_confirm street city emt_city emt_county verification_emt_number).each do |field|
         fill_in field, :with => data.fetch(field)
     end
 
-    %w(birth_date zip emt_zip).each do |field|
+    %w(verification_birth_date zip emt_zip).each do |field|
       2.times {fill_in field, :with => data.fetch(field)}
     end
 
     escape_google_address_autocomplete(%w(#street #emt_city))
 
-    #Added the 2 lines above populate_second_state to deal with the random dropdown not working on occasion
     all("#s2id_state")[0].click
-    find("#s2id_autogen2_search").native.send_keys :escape
-    2.times {populate_second_state("Kansas", index=0)}
+    pick_result("Kansas")
   end
 
-  def container_attribute
-    "emt"
-  end
+  # def container_attribute
+  #   "emt"
+  # end
 
-  def unique_id
-    "#s2id_emt_state"
-  end
+  # def unique_id
+  #   "#s2id_emt_state"
+  # end
 
   def required_fields
     [0,1,2,3,4,5,6,7,9,10,11,12]
+  end
+
+  def click_verify_emt_state_link
+    click_link("Verify as a certified EMT / Paramedic")
+    click_link("I am state certified")
+    select_option("#s2id_state","Alaska")
   end
 
 end
