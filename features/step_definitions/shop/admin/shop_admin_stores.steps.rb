b73283ab5a9c7585afa3c27d7f29840cc61bb9c8
Eddie Park <eddie.park@id.me>
2017-06-14 08:43:21 -0400

    IDME-3714: Add category to shop admin stores step defs

--- features/step_definitions/shop/admin/shop_admin_stores.steps.rb
+++ features/step_definitions/shop/admin/shop_admin_stores.steps.rb
@@ -10,3 +10,27 @@ end
 Given(/^I verify the store has been created$/) do
   expect(page).to have_css(".alert-success", :text => "Store successfully created.")
 end
+
+Given(/^I click to edit a store from the shop admin stores index$/) do
+  @ShopAdminStores.search_for_store
+  @ShopAdminStores.select_first_matching_store
+end
+
+Given(/^I add a new category to the store$/) do
+  @ShopAdminStores.search_for_store_category
+  @ShopAdminStores.select_category_result_return
+  @ShopAdminStores.update_store_page
+end
+
+Given(/^I verify the store has been updated$/) do
+  expect(page).to have_css(".alert-success", :text => "× Store successfully updated.")
+end
+
+Given(/^I verify the category has been added to the store$/) do
+  expect(page).to have_css("#store_categories_attributes_0_label[value='Fitness & Diet']")
+end
+
+Given(/^I delete the store category$/) do
+  @ShopAdminStores.remove_store_category
+  @ShopAdminStores.update_store_page
+end
