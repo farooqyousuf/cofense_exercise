751cc93541d7ea7a515017523b8ff6177b9066b6
pamelaolney <pjolney12@gmail.com>
2017-06-19 17:53:42 -0400

    CORE-7669: Updated required fields

--- features/support/app_pages/iva/military_dd214_vet.rb
+++ features/support/app_pages/iva/military_dd214_vet.rb
@@ -97,7 +97,7 @@ class DD214 < IDmeBase
   end
 
   def required_fields
-    [0, 1, 2, 3, 4, 5, 6, 8, 10, 11, 12, 14, 15, 16, 17]
+    [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
   end
 
   def click_verify_by_dd214_link
