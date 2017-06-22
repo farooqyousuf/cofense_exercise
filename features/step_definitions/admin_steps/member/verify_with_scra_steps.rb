d4ee780e4490babff4cb09f1d2397c2d1f65956b
pamelaolney <pjolney12@gmail.com>
2017-06-22 16:44:43 -0400

    INT-1432: Added step to view errors for required fields

--- features/step_definitions/admin_steps/member/verify_with_scra_steps.rb
+++ features/step_definitions/admin_steps/member/verify_with_scra_steps.rb
@@ -11,3 +11,16 @@ Given(/^I should be successfully verified as a "([^"]*)"$/) do |group|
   username = @username
   VerifyWithScra.new.verify_scra_applied(@username, affiliation: group)
 end
+
+Given(/^I should see errors on required fields for "([^"]*)" in Admin tool$/) do |affiliation|
+  form_field = %w[service_member_first_name service_member_last_name service_member_birth_date social service_date]
+  form_field_family = %w[first_name last_name birth_date]
+
+  fields = case affiliation
+           when "Service Member", "Veteran", "Retiree"    then form_field
+           when "Military Family", "Military Spouse"      then form_field.push(*form_field_family)
+           else fail("Affiliation not found")
+           end
+
+  VerifyWithScra.new.check_admin_scra_form_error(fields, affiliation: affiliation)
+end
