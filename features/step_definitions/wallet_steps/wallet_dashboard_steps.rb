0e0297c1d5b06332857c48e380d217be99450939
luminousbeam <jocelyn.jeriah@gmail.com>
2017-07-26 17:40:23 -0400

    IDME-3746: Add corresponding capybara steps to support shared nav scenario

--- features/step_definitions/wallet_steps/wallet_dashboard_steps.rb
+++ features/step_definitions/wallet_steps/wallet_dashboard_steps.rb
@@ -11,3 +11,27 @@ end
 Given(/^I click sign out on the shared side navigation$/) do
   @WalletDashboard.click_shared_side_navigation_sign_out
 end
+
+Given(/^I verify ID\.me logo is present$/) do
+  expect(page).to have_css("img.shared-nav-logo")
+ end
+
+Given(/^I verify that the main nav Shop link takes me to the correct page$/) do
+  @WalletDashboard.click_on_shop_shared_nav_tab
+  expect(page).to have_current_path(FigNewton.shop.shop_homepage, :url => true)
+end
+
+Given(/^I verify that the main nav About link takes me to the correct page$/) do
+  @WalletDashboard.click_on_about_shared_nav_tab
+  expect(page).to have_current_path(FigNewton.idme.about_page, :url => true)
+end
+
+Given(/^I verify that the main nav Wallet link takes me to the correct page$/) do
+  @WalletDashboard.click_on_wallet_shared_nav_tab_from_about_page
+  expect(page).to have_current_path(FigNewton.wallet.homepage.url, :url => true)
+end
+
+Given(/^I verify that all the main nav tab links are directing to correct pages$/) do
+  @WalletDashboard.click_on_business_shared_nav_tab
+  expect(page).to have_current_path(FigNewton.idme.business_page, :url => true)
+end
--- features/support/app_pages/wallet/wallet_dashboard.rb
+++ features/support/app_pages/wallet/wallet_dashboard.rb
@@ -1,19 +1,35 @@
 class WalletDashboard < IDmeBase
   include IDPBase
-  
+
   def initialize
     super(FigNewton.wallet.dashboard)
   end
 
   def open_shared_side_navigation
    find(".shared-nav-user-avatar").click
-  end 
+  end
 
   def close_shared_side_navigation
   	find(".icon-wallet-close").click
-  end 
+  end
 
   def click_shared_side_navigation_sign_out
     click_link "Sign Out"
-  end 
-end 
+  end
+
+  def click_on_about_shared_nav_tab
+    find("ul.shared-nav-tabs li:nth-child(1)").click
+  end
+
+  def click_on_business_shared_nav_tab
+    find("ul.shared-nav-tabs li:nth-child(2)").click
+  end
+
+  def click_on_shop_shared_nav_tab
+    find("ul.shared-nav-tabs li:nth-child(3)").click
+  end
+
+  def click_on_wallet_shared_nav_tab_from_about_page
+    find("nav.wallet-header-nav li:nth-child(4)").click
+  end
+end
