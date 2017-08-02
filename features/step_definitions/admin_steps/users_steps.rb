3d85e1cf00ee48d29f6575d3449064b49f097e7a
Farooq <farooqyousuf1@gmail.com>
2017-08-02 12:26:15 -0400

    INT-1711: Cleaned up small things

--- features/step_definitions/admin_steps/users_steps.rb
+++ features/step_definitions/admin_steps/users_steps.rb
@@ -1,7 +1,6 @@
 Given(/^I verify user level properties for "([^"]*)"$/) do |affiliation|
   visit_admin_users_in_new_window
   sleep 3
-  all('td')[0].text
   all('td')[0].click #click on the very first user in the table
 
   @user_edit = UserEdit.new
