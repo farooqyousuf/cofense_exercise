276e3fcca91ef984b158ca745c79b03fbe0080e9
pamelaolney <pjolney12@gmail.com>
2017-08-21 15:35:02 -0400

    INT-1777: Added tests to send repsonse with missing attributes

--- features/direct_api_features/direct_api_client_cred.feature
+++ features/direct_api_features/direct_api_client_cred.feature
@@ -4,3 +4,8 @@ Feature: Test Direct API
     * I send a POST request with valid Client Credential attributes to ARCS staging
     * I verify the user's military attributes
     * I verify that I can authenticate with valid Client Credentials
+
+  Scenario: Successful Client Credential error response for missing attributes
+    * I send a POST request with missing Client Credential attributes to ARCS staging
+    * I verify the user's missing military attributes
+    * I verify that I authenticated with missing Client Credentials
--- features/step_definitions/direct_api_steps/direct_api_steps.rb
+++ features/step_definitions/direct_api_steps/direct_api_steps.rb
@@ -15,3 +15,18 @@ Given(/^I verify that I can authenticate with valid Client Credentials$/) do
   expect(@response_values).to eq(@expected_values)  # verifies response values match up with the yml file
   puts @response
 end
+
+Given(/^I send a POST request with missing Client Credential attributes to ARCS staging$/) do
+  @response = HTTParty.post("#{FigNewton.oauth.idp_endpoint}/api/direct/v2/verify",
+                            :query => { })
+end
+
+Given(/^I verify the user's missing military attributes$/) do
+  expect(@response.success?).to eq(false) # verifies status to not equal 200
+  expect(@response["code"]).to eq("122")  # verifies the code attribute value is 122
+end
+
+Given(/^I verify that I authenticated with missing Client Credentials$/) do
+  expect(@response_values).to eq(nil)
+  puts @response
+end
