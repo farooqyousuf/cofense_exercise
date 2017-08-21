8b2e7c2752906eb7e63e61d96b4ab44406176140
pamelaolney <pjolney12@gmail.com>
2017-08-21 17:23:50 -0400

    INT-1777: Added case methods

--- features/step_definitions/direct_api_steps/direct_api_steps.rb
+++ features/step_definitions/direct_api_steps/direct_api_steps.rb
@@ -1,32 +1,44 @@
-Given(/^I send a POST request with valid Client Credential attributes to ARCS staging$/) do
+Given(/^I send a POST request with "([^"]*)" attributes to ARCS staging$/) do |request|
+  attributes = case request
+               when "valid Client Credential"     then FigNewton.direct_api.to_hash
+               when "missing Client Credential"   then {}
+               else fail ("Failed POST request.")
+               end
+
   @response = HTTParty.post("#{FigNewton.oauth.idp_endpoint}/api/direct/v2/verify",
-                            :query => FigNewton.direct_api.to_hash)
-end
-
-Given(/^I verify the user's military attributes$/) do
-  expect(@response.success?).to eq(true)  # verifies status is 200
-  expect(@response["code"]).to eq(nil)    # verifies the code attribute key is nil
-
-  @expected_values = FigNewton.direct_api.to_hash.except("client_id", "client_secret")
-  @response_values = @response.to_hash.except("service_branch", "service_name", "service_component", "service_end_date", "service_start_date", "verified", "affiliation", "external")
-end
-
-Given(/^I verify that I can authenticate with valid Client Credentials$/) do
-  expect(@response_values).to eq(@expected_values)  # verifies response values match up with the yml file
+                            :query => attributes)
   puts @response
 end
 
-Given(/^I send a POST request with missing Client Credential attributes to ARCS staging$/) do
-  @response = HTTParty.post("#{FigNewton.oauth.idp_endpoint}/api/direct/v2/verify",
-                            :query => { })
+Given(/^I verify the user's "([^"]*)" military attributes$/) do |method|
+  case method
+  when "valid"
+    expect(@response.success?).to eq(true)
+    expect(@response["code"]).to eq(nil)
+  when "missing"
+    expect(@response.success?).to eq(false)
+    expect(@response["code"]).to eq("122")
+  else fail ("Method not found")
+  end
 end
 
-Given(/^I verify the user's missing military attributes$/) do
-  expect(@response.success?).to eq(false) # verifies status to not equal 200
-  expect(@response["code"]).to eq("122")  # verifies the code attribute value is 122
-end
+Given(/^I verify that I authenticated with "([^"]*)" attributes$/) do |request|
+# response_values incldue client_id, client_secret, first_name, last_name, birth_date and ssn
+  response_values = @response.to_hash.except("service_branch",
+                                              "service_name",
+                                              "service_component",
+                                              "service_end_date",
+                                              "service_start_date",
+                                              "verified",
+                                              "affiliation",
+                                              "external",
+                                              "message",
+                                              "code")
 
-Given(/^I verify that I authenticated with missing Client Credentials$/) do
-  expect(@response_values).to eq(nil)
-  puts @response
+  expected_values = case request
+                    when "valid Client Credential"    then FigNewton.direct_api.to_hash.except("client_id", "client_secret")
+                    when "missing Client Credential"  then {}
+                    else fail ("Failed authentication with #{request} attribute.")
+                    end
+  expect(response_values).to eq(expected_values)
 end
