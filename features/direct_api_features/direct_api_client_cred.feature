fdc0cd4a3ad3092a717ea882896de3329d03d2fa
pamelaolney <pjolney12@gmail.com>
2017-08-21 11:29:54 -0400

    INT-1776: Updated direct api steps and expect methods

--- features/direct_api_features/direct_api_client_cred.feature
+++ features/direct_api_features/direct_api_client_cred.feature
@@ -2,4 +2,5 @@ Feature: Test Direct API
 
   Scenario: Successful Client Credential POST request sent to ARCS staging
     * I send a POST request with valid Client Credential attributes to ARCS staging
-    * I verify that the response is valid
+    * I verify the user's military attributes
+    * I verify that I can authenticate with valid Client Credentials
--- features/step_definitions/direct_api_steps/direct_api_steps.rb
+++ features/step_definitions/direct_api_steps/direct_api_steps.rb
@@ -3,13 +3,15 @@ Given(/^I send a POST request with valid Client Credential attributes to ARCS st
                             :query => FigNewton.direct_api.to_hash)
 end
 
-Given(/^I verify that the response is valid$/) do
-  expect(@response.success?).to be(true)  # verifies status is 200
-  expect(@response["code"]).to be(nil)    # verifies the code attribute key is nil
+Given(/^I verify the user's military attributes$/) do
+  expect(@response.success?).to eq(true)  # verifies status is 200
+  expect(@response["code"]).to eq(nil)    # verifies the code attribute key is nil
 
-  expected_values = FigNewton.direct_api.to_hash.except("client_id", "client_secret")
-  response_values = @response.to_hash.except("service_branch", "service_name", "service_component", "service_end_date", "service_start_date", "verified", "affiliation", "external")
+  @expected_values = FigNewton.direct_api.to_hash.except("client_id", "client_secret")
+  @response_values = @response.to_hash.except("service_branch", "service_name", "service_component", "service_end_date", "service_start_date", "verified", "affiliation", "external")
+end
 
-  expect(response_values).to eq(expected_values)  # verifies response values match up with the yml file
+Given(/^I verify that I can authenticate with valid Client Credentials$/) do
+  expect(@response_values).to eq(@expected_values)  # verifies response values match up with the yml file
   puts @response
 end
