fdc036f5277deb5121c13373feb05a4b12074746
pamelaolney <pjolney12@gmail.com>
2017-08-22 12:38:36 -0400

    INT-1777: Removed step and updated expect methods

--- features/step_definitions/direct_api_steps/direct_api_steps.rb
+++ features/step_definitions/direct_api_steps/direct_api_steps.rb
@@ -13,35 +13,11 @@ end
 Given(/^I verify that I authenticated with "([^"]*)" attributes$/) do |method|
   case method
   when "valid Client Credential"
-    expected_status = true
-    expected_code = nil
+    expect(@response.code).to eq(200)             # verifies HTTP status code
+    expect(@response["code"]).not_to eq("122")    # verifies response code attribute
   when "missing Client Credential"
-    expected_status = false
-    expected_code = "122"
+    expect(@response.code).to eq(403)
+    expect(@response["code"]).to eq("122")
   else fail ("Method not found")
   end
-
-  expect(@response.success?).to eq(expected_status)
-  expect(@response["code"]).to eq(expected_code)
-end
-
-Given(/^I verify the user's "([^"]*)" military attributes$/) do |request|
-# response_values incldue client_id, client_secret, first_name, last_name, birth_date and ssn
-  response_values = @response.to_hash.except("service_branch",
-                                              "service_name",
-                                              "service_component",
-                                              "service_end_date",
-                                              "service_start_date",
-                                              "verified",
-                                              "affiliation",
-                                              "external",
-                                              "message",
-                                              "code")
-
-  expected_values = case request
-                    when "valid"    then FigNewton.direct_api.to_hash.except("client_id", "client_secret")
-                    when "missing"  then {}
-                    else fail ("Failed authentication with #{request} attribute.")
-                    end
-  expect(response_values).to eq(expected_values)
 end
