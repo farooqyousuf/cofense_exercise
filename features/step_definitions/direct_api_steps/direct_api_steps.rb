643e92362e3ec41dc4949772ab3bdf3e279d865c
pamelaolney <pjolney12@gmail.com>
2017-08-31 17:13:02 -0400

    INT-1778: Divided steps and yml file

--- features/step_definitions/direct_api_steps/direct_api_steps.rb
+++ features/step_definitions/direct_api_steps/direct_api_steps.rb
@@ -1,13 +1,22 @@
-Given(/^I send a POST request with "([^"]*)" attributes to ARCS staging$/) do |request|
-  attributes = case request
-               when "valid"                 then FigNewton.direct_api_client_cred.valid_attributes.to_hash
-               when "invalid"               then FigNewton.direct_api_client_cred.invalid_attributes.to_hash
-               when "missing"               then {}
-               else fail ("Failed POST request with #{request}.")
-               end
+Given(/^I used "([^"]*)" client credentials$/) do |credentials|
+  @request_params = case credentials
+                    when "valid" then { "client_id"     => FigNewton.direct_api_client_cred.client_id,
+                                        "client_secret" => FigNewton.direct_api_client_cred.client_secret}
+                    end
+end
 
+Given(/^I used "([^"]*)" user attributes$/) do |credentials|
+  case credentials
+  when "valid"    then @request_params.merge!(FigNewton.direct_api_client_cred.valid_attributes.to_hash)
+  when "invalid"  then @request_params.merge!(FigNewton.direct_api_client_cred.invalid_attributes.to_hash)
+  when "missing"  then @request_params.merge!({ }.to_hash)
+  end
+end
+
+
+Given(/^I send a POST request to ARCS$/) do
   @response = HTTParty.post("#{FigNewton.oauth.idp_endpoint}/api/direct/v2/verify",
-                            :query => attributes)
+                            :query => @request_params)
   puts @response
 end
 
@@ -33,6 +42,19 @@ Given(/^I verify the user's "([^"]*)" military attributes is received$/) do |req
                           "code"               => 1001,
                           "verified"           => false
                         }
+                      when "missing"
+                        {
+                          "code"               => 1002,
+                          "error"              => {
+                                                    "first_name" => ["can't be blank"],
+                                                    "last_name"  => ["can't be blank"],
+                                                    "ssn"        => ["can't be blank",
+                                                                     "is the wrong length (should be 9 characters)",
+                                                                     "is not a number"],
+                                                    "birth_date" => ["can't be blank",
+                                                                     "must be in yyyy-mm-dd format"],
+                                                   }
+                        }
                       else fail ("Unable to find the requested #{request} attributes")
                       end
 
@@ -44,12 +66,12 @@ Given(/^I verify that I authenticated with "([^"]*)" attributes$/) do |method|
   when "valid"
     expect(@response.code).to eq(200)             # verifies HTTP status code
     expect(@response["code"]).not_to eq("122")    # verifies response code attribute
-  when "missing"
-    expect(@response.code).to eq(403)
-    expect(@response["code"]).to eq("122")
   when "invalid"
     expect(@response.code).to eq(200)
     expect(@response["code"]).to eq(1001)
+  when "missing"
+    expect(@response.code).to eq(400)
+    expect(@response["code"]).to eq(1002)
   else fail ("Method for #{method} not found")
   end
 end
