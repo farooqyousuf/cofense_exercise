fe9e2868f994c25cde7ec7bbcbc221371ef1063b
pamelaolney <pjolney12@gmail.com>
2017-08-31 14:19:19 -0400

    INT-1778: Updated methods and yml data

--- features/direct_api_features/direct_api_client_cred.feature
+++ features/direct_api_features/direct_api_client_cred.feature
@@ -4,10 +4,6 @@ Feature: Test Direct API
     * I send a POST request with "valid" attributes to ARCS staging
     * I verify the user's "valid" military attributes is received
 
-  Scenario: Successfully receive missing attributes of the user verified against SCRA with the Direct API
-    * I send a POST request with "missing" attributes to ARCS staging
-    * I verify the user's "invalid" military attributes is received
-
   Scenario: Successfully receive invalid attributes of the user verified against SCRA with the Direct API
     * I send a POST request with "invalid" attributes to ARCS staging
     * I verify the user's "invalid" military attributes is received
@@ -18,7 +14,7 @@ Feature: Test Direct API
 
   Scenario: Successfully POST missing attributes to ARCS staging
     * I send a POST request with "missing" attributes to ARCS staging
-    * I verify that I authenticated with "invalid" attributes
+    * I verify that I authenticated with "missing" attributes
 
   Scenario: Successfully POST invalid attributes to ARCS staging
     * I send a POST request with "invalid" attributes to ARCS staging
--- features/step_definitions/direct_api_steps/direct_api_steps.rb
+++ features/step_definitions/direct_api_steps/direct_api_steps.rb
@@ -1,11 +1,24 @@
+Given(/^I send a POST request with "([^"]*)" attributes to ARCS staging$/) do |request|
+  attributes = case request
+               when "valid"                 then FigNewton.direct_api_client_cred.valid_attributes.to_hash
+               when "invalid"               then FigNewton.direct_api_client_cred.invalid_attributes.to_hash
+               when "missing"               then {}
+               else fail ("Failed POST request with #{request}.")
+               end
+
+  @response = HTTParty.post("#{FigNewton.oauth.idp_endpoint}/api/direct/v2/verify",
+                            :query => attributes)
+  puts @response
+end
+
 Given(/^I verify the user's "([^"]*)" military attributes is received$/) do |request|
   expected_response = case request
                       when "valid"
                         {
-                          "first_name"         => FigNewton.direct_api.first_name,
-                          "last_name"          => FigNewton.direct_api.last_name,
-                          "birth_date"         => FigNewton.direct_api.birth_date,
-                          "ssn"                => FigNewton.direct_api.ssn,
+                          "first_name"         => FigNewton.direct_api_client_cred.valid_attributes.first_name,
+                          "last_name"          => FigNewton.direct_api_client_cred.valid_attributes.last_name,
+                          "birth_date"         => FigNewton.direct_api_client_cred.valid_attributes.birth_date,
+                          "ssn"                => FigNewton.direct_api_client_cred.valid_attributes.ssn,
                           "service_branch"     => "Army",
                           "service_component"  => "active",
                           "service_end_date"   => nil,
@@ -16,26 +29,14 @@ Given(/^I verify the user's "([^"]*)" military attributes is received$/) do |req
                         }
                       when "invalid"
                         {
-                          "message"            => "Consumer is not allowed to access this API",
-                          "code"               => "122"
+                          "message"            => "We are unable to verify the given credentials.",
+                          "code"               => 1001,
+                          "verified"           => false
                         }
-                      else fail ("Unable to find the rquested #{request} attributes")
+                      else fail ("Unable to find the requested #{request} attributes")
                       end
 
   expect(@response.to_hash).to eq(expected_response)
-end
-
-Given(/^I send a POST request with "([^"]*)" attributes to ARCS staging$/) do |request|
-  attributes = case request
-               when "valid"                 then FigNewton.direct_api.to_hash
-               when "missing"               then {}
-               when "invalid"               then FigNewton.invalid_id_secret_direct_api.to_hash
-               else fail ("Failed POST request with #{request}.")
-               end
-
-  @response = HTTParty.post("#{FigNewton.oauth.idp_endpoint}/api/direct/v2/verify",
-                            :query => attributes)
-  puts @response
 end
 
 Given(/^I verify that I authenticated with "([^"]*)" attributes$/) do |method|
@@ -43,9 +44,12 @@ Given(/^I verify that I authenticated with "([^"]*)" attributes$/) do |method|
   when "valid"
     expect(@response.code).to eq(200)             # verifies HTTP status code
     expect(@response["code"]).not_to eq("122")    # verifies response code attribute
-  when "invalid"
+  when "missing"
     expect(@response.code).to eq(403)
     expect(@response["code"]).to eq("122")
+  when "invalid"
+    expect(@response.code).to eq(200)
+    expect(@response["code"]).to eq(1001)
   else fail ("Method for #{method} not found")
   end
 end
