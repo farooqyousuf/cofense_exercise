87fb9adfeccc6af19692380fe945f1ec96682e10
pamelaolney <pjolney12@gmail.com>
2017-09-11 09:55:04 -0400

    INT-1779: Successful Client Credential error response for incorrect format
    INT-1779: Style cleanup
    
    INT-1779: Style cleanup

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -4,6 +4,9 @@
 
 All notable changes to this project will be documented in this file. This project does adhere to [Semantic Versioning](http://semver.org/)
 
+# 2.4.17 (9/11/17)
+- INT-1779: Successful Client Credential error response for incorrect format
+
 # 2.4.16 (9/05/17)
 - MOB-963: Android Native Mobile setup complete
 
--- features/step_definitions/direct_api_steps/client_credential_steps.rb
+++ features/step_definitions/direct_api_steps/client_credential_steps.rb
@@ -1,6 +1,6 @@
 Given(/^I set valid client credentials$/) do
   @request_params = { "client_id"      => FigNewton.direct_api_client_cred.client_id,
-                       "client_secret" => FigNewton.direct_api_client_cred.client_secret }
+                      "client_secret"  => FigNewton.direct_api_client_cred.client_secret }
 end
 
 Given(/^I set "([^"]*)" user attributes$/) do |credentials|
@@ -8,6 +8,14 @@ Given(/^I set "([^"]*)" user attributes$/) do |credentials|
                     when "valid"   then FigNewton.direct_api_client_cred.valid_attributes.to_hash
                     when "invalid" then FigNewton.direct_api_client_cred.invalid_attributes.to_hash
                     when "missing" then {}
+                    when "incorrect format"
+                      {
+                        "first_name" => FigNewton.direct_api_client_cred.valid_attributes.first_name,
+                        "last_name"  => FigNewton.direct_api_client_cred.valid_attributes.last_name,
+                        "birth_date" => "05-25-1989",
+                        "ssn"        => "OOO149876d"
+                      }
+                    else fail ("Unable to find #{credentials} user attributes")
                     end
 
   @request_params.merge!(credential_hash)
@@ -42,16 +50,24 @@ Given(/^I verify the user's "([^"]*)" military attributes is received$/) do |req
                         }
                       when "missing"
                         {
-                          "code"               => 1002,
-                          "error"              => {
-                                                    "first_name" => ["can't be blank"],
-                                                    "last_name"  => ["can't be blank"],
-                                                    "ssn"        => ["can't be blank",
-                                                                     "is the wrong length (should be 9 characters)",
-                                                                     "is not a number"],
-                                                    "birth_date" => ["can't be blank",
-                                                                     "must be in yyyy-mm-dd format"],
-                                                   }
+                          "code"  => 1002,
+                          "error" => {
+                                       "first_name" => ["can't be blank"],
+                                       "last_name"  => ["can't be blank"],
+                                       "ssn"        => ["can't be blank",
+                                                        "is the wrong length (should be 9 characters)",
+                                                        "is not a number"],
+                                       "birth_date" => ["can't be blank",
+                                                        "must be in yyyy-mm-dd format"]
+                                      }
+                        }
+                      when "incorrect format"
+                        {
+                          "code"  => 1002,
+                          "error" => {
+                                      "ssn"         => ["is the wrong length (should be 9 characters)",
+                                                        "is not a number"],
+                                       "birth_date" => ["must be in yyyy-mm-dd format"]}
                         }
                       else fail ("Unable to find the requested #{request} attributes")
                       end
@@ -69,7 +85,7 @@ Given(/^I verify that I authenticated with "([^"]*)" attributes$/) do |method|
     expect(@response.code).to eq(200)
     expect(@response["code"]).to eq(1001)
 
-  when "missing"
+  when "missing", "incorrect format"
     expect(@response.code).to eq(400)
     expect(@response["code"]).to eq(1002)
 
