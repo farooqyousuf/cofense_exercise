86ead7227c2147197bb621857808bd44de103db2
Eddie Park <eddie.park@id.me>
2017-10-03 11:37:08 -0400

    IDME-4006: Add step def for create email generator feature

---
+++ features/step_definitions/shop/admin/shop_admin_email_generator_steps.rb
@@ -0,0 +1,11 @@
+Given(/^I navigate to the create a new email template page$/) do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Given(/^I submit a valid new email template$/) do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Given(/^I verify that the new email template has been saved$/) do
+  pending # Write code here that turns the phrase above into concrete actions
+end
