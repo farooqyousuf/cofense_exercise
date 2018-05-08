0f181e389a3c729a8d679a3b6f7be031630550ee
pamelaolney <pjolney12@gmail.com>
2018-05-08 11:10:39 -0400

    AWE-698: Simplified steps

--- features/imagery_features/imagery.feature
+++ features/imagery_features/imagery.feature
@@ -1,10 +1,13 @@
 Feature: Compare AAMVA file to source document
 
-  Scenario: Successfully compare source document to self
-    * I compare an "exact_match" csv file against the source document
-
-  Scenario: Compare source document with missing driver's licenses data
-    * I compare an "missing_dl" csv file against the source document
+  Scenario: Exact match
+    * I compare the master aamva file against a "exact_match" file
 
   Scenario: Missing field
-    * I compare the master aamva file against an aamva file that has a missing field
+    * I compare the master aamva file against a "missing_field" file
+
+  Scenario: Missing driver's license
+    * I compare the master aamva file against a "missing_dl" file
+
+  Scenario: Missing state
+    * I compare the master aamva file against a "missing_state" file
--- features/step_definitions/imagery_steps/imagery_steps.rb
+++ features/step_definitions/imagery_steps/imagery_steps.rb
@@ -1,33 +1,17 @@
-Given("I compare an {string} csv file against the source document") do |file|
-  original = CSV.read("test_documents/aamva_master.txt")
-  new_file = CSV.read("test_documents/aamva_#{file}.txt")
-  case file
-  when "exact_match"
-    flag = expect(original).to eq(original)
-    if flag == true
-      puts "Both AAMVA files are an exact match"
-    else
-      puts "The AAMVA file is #{file}"
-      expect(new_file).not_to eq(original)
-    end
-  end
-end
-
-
-Given("I compare the master aamva file against an aamva file that has a missing field") do
+Given("I compare the master aamva file against a {string} file") do |file|
   lines_master = File.foreach("test_documents/aamva_master.txt").count
-  lines_missing_field = File.foreach("test_documents/aamva_missing_field.txt").count
+  lines_diff = File.foreach("test_documents/aamva_#{file}.txt").count
 
   master = File.readlines("test_documents/aamva_master.txt")
-  new_file = File.readlines("test_documents/aamva_missing_field.txt")
+  new_file = File.readlines("test_documents/aamva_#{file}.txt")
 
-  if lines_master == lines_missing_field
-    puts "There is no missing field in the new file."
+  if lines_master == lines_diff
+    puts "The #{file} file is an exact match to master."
   else
-    puts "There is one or more missing fields in the new file."
+    puts "There is one or more #{file} in the new file."
     diff = master - new_file
     puts "Master AAMVA file has #{lines_master} lines."
-    puts "The new AAMVA file has #{lines_missing_field} lines."
-    puts "The missing field is #{diff}"
+    puts "The new AAMVA file has #{lines_diff} lines."
+    puts "The #{file} is #{diff}"
   end
 end
