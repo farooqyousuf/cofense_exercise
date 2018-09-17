036db10c907794b2e25e2577f5f0d1b648768433
pamelaolney <pjolney12@gmail.com>
2018-09-17 17:06:02 -0400

    AWE-1795: Working on verifying updated user

--- features/step_definitions/support_tool_steps/support_tool_steps.rb
+++ features/step_definitions/support_tool_steps/support_tool_steps.rb
@@ -12,3 +12,17 @@ Given("I verify that the new user is no longer found within the Support tool") d
   @SupportToolVerificationAttempts.search_user_by_email(email_address: @user_email)
   expect(page).not_to have_css(".odd > td", :text => @user_email)
 end
+
+Given("I update the {string} verification attempt within the IDme support tool") do |type|
+  @SupportTool.login_in_new_window
+  step 'I visit "SupportToolVerificationAttempts"'
+  @SupportToolVerificationAttempts.open_newest
+  @SupportToolEditAttemps.edit_attempt(type: type)
+end
+
+Given("I verify the updated {string} verification attempt within the IDme support tool") do |type|
+  step 'I visit "SupportTool"'
+  @SupportToolVerificationAttempts.search_user_by_email(email_address: @user_email)
+  @SupportToolVerificationAttempts.open_newest
+  binding.pry
+end
